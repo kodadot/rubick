a5dbfa0e842c1de5a7fe01bf75246ef7793f7402
Viki Val <viktorko99@gmail.com>
2021-12-11 10:53:09 +0100

    :broom: fromated index, created mainframe for processing

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,85 +1,97 @@
 import { CollectionEntity, NFTEntity } from '../generated/model'
 
-import { getRemarksFrom, processBatch, RemarkResult } from './utils';
-import { Collection, eventFrom, getNftId, NFT, RmrkEvent, RmrkInteraction } from './utils/types';
-import NFTUtils, { hexToString } from './utils/NftUtils';
-import { canOrElseError, exists, hasMeta, isBurned, isBuyLegalOrElseError, isOwnerOrElseError, isPositiveOrElseError, isTransferable, validateInteraction } from './utils/consolidator'
+import { extractRemark, Records } from './utils'
+import {
+  Collection,
+  eventFrom,
+  getNftId,
+  NFT,
+  RmrkEvent,
+  RmrkInteraction,
+} from './utils/types'
+import NFTUtils, { hexToString } from './utils/NftUtils'
+import {
+  canOrElseError,
+  exists,
+  hasMeta,
+  isBurned,
+  isBuyLegalOrElseError,
+  isOwnerOrElseError,
+  isPositiveOrElseError,
+  isTransferable,
+  validateInteraction,
+} from './utils/consolidator'
 import { randomBytes } from 'crypto'
-import { emoteId, ensureInteraction } from './utils/helper';
-import { System, Utility } from '../types';
-import { Context } from './utils/types';
+import { emoteId, ensureInteraction } from './utils/helper'
+import { System, Utility } from '../types'
+import { Context } from './utils/types'
 
-
-export async function handleRemark(context: Context): Promise<void> {
-  // // const records = getRemarksFrom(extrinsic)
-
+export function handleRemark(context: Context): void {
   const remark = new System.RemarkCall(context.extrinsic).remark
-  const decoded = hexToString(remark.toString())
-  console.log('remark', decoded)
-
-
-
-  // for (const remark of records) {
-  //   console.log(`Handling remark ${remark.value} ${remark.blockNumber}`)
-  //   try {
-  //     const decoded = hexToString(remark.value)
-  //     const event: RmrkEvent = NFTUtils.getAction(decoded)
-
-  //     switch (event) {
-  //       case RmrkEvent.MINT:
-  //         // await mint(remark)
-  //         break;
-  //       case RmrkEvent.MINTNFT:
-  //         // await mintNFT(remark)
-  //         break;
-  //       case RmrkEvent.SEND:
-  //         // await send(remark)
-  //         break;
-  //       case RmrkEvent.BUY:
-  //         // await buy(remark)
-  //         break;
-  //       case RmrkEvent.CONSUME:
-  //         // await consume(remark)
-  //         break;
-  //       case RmrkEvent.LIST:
-  //         // await list(remark)
-  //         break;
-  //       case RmrkEvent.CHANGEISSUER:
-  //         // await changeIssuer(remark)
-  //         break;
-  //       case RmrkEvent.EMOTE:
-  //         // await emote(remark)
-  //         break;
-  //       default:
-  //         // logger.warn(`[SKIP] ${event}::${remark.value}::${remark.blockNumber}`)
-  //         // throw new EvalError(`Unable to evaluate following string, ${event}::${remark.value}`)
-  //     }
-  //   } catch (e) {
-  //     // logger.error(`[MALFORMED] ${remark.blockNumber}::${hexToString(remark.value)}`)
-  //   }
-      
-  // }
+  const records = extractRemark(remark.toString(), context)
+  mainFrame(records)
 }
 
-export async function handleBatch(context: Context): Promise<void> {
+export function handleBatch(context: Context): void {
   const batch = new Utility.BatchCall(context.extrinsic).calls
-  console.log('handleBatch', batch.toString())
-  // processBatch(batch)
+  const records = extractRemark(batch.toString(), context)
+  mainFrame(records)
 }
 
-
-export async function handleBatchAll(context: Context): Promise<void> {
+export function handleBatchAll(context: Context): void {
   const batch = new Utility.Batch_allCall(context.extrinsic).calls
-  console.log('handleBatchAll', batch.toString())
+  const records = extractRemark(batch, context)
+  mainFrame(records)
 }
 
+async function mainFrame(records: Records): Promise<void> {
+  for (const remark of records) {
+    console.log(`Handling remark ${remark.value} ${remark.blockNumber}`)
+    try {
+      const decoded = hexToString(remark.value)
+      const event: RmrkEvent = NFTUtils.getAction(decoded)
+
+      switch (event) {
+        case RmrkEvent.MINT:
+          // await mint(remark)
+          break
+        case RmrkEvent.MINTNFT:
+          // await mintNFT(remark)
+          break
+        case RmrkEvent.SEND:
+          // await send(remark)
+          break
+        case RmrkEvent.BUY:
+          // await buy(remark)
+          break
+        case RmrkEvent.CONSUME:
+          // await consume(remark)
+          break
+        case RmrkEvent.LIST:
+          // await list(remark)
+          break
+        case RmrkEvent.CHANGEISSUER:
+          // await changeIssuer(remark)
+          break
+        case RmrkEvent.EMOTE:
+          // await emote(remark)
+          break
+        default:
+        // logger.warn(`[SKIP] ${event}::${remark.value}::${remark.blockNumber}`)
+        // throw new EvalError(`Unable to evaluate following string, ${event}::${remark.value}`)
+      }
+    } catch (e) {
+      // logger.error(`[MALFORMED] ${remark.blockNumber}::${hexToString(remark.value)}`)
+    }
+  }
+}
 
 // export async function balancesTransfer({
 //   store,
 //   event,
 //   block,
 //   extrinsic,
-// }: EventContext & StoreContext): Promise<void> {
+// }: EventContext & StoreContext): void {
 
 //   const [from, to, value] = new Balances.TransferEvent(event).params
 //   const tip = extrinsic?.tip || 0n
@@ -110,7 +122,6 @@ export async function handleBatchAll(context: Context): Promise<void> {
 //   await store.save(hbTo)
 // }
 
-
 // async function getOrCreate<T extends {id: string}>(
 //   store: DatabaseManager,
 //   entityConstructor: EntityConstructor<T>,
@@ -128,7 +139,6 @@ export async function handleBatchAll(context: Context): Promise<void> {
 
 //   return e
 // }
-
 
 type EntityConstructor<T> = {
   new (...args: any[]): T
