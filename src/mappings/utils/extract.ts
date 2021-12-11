460cb345d0bbef2cfb69d98d59a418594f0ff837
Viki Val <viktorko99@gmail.com>
2021-12-11 09:57:56 +0100

    :broom: cleanup in the extract

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,10 +1,9 @@
 import { Vec } from '@polkadot/types';
 import { Call as TCall } from "@polkadot/types/interfaces";
-import { EventRecord, Event } from '@polkadot/types/interfaces';
-import { ExtrinsicContext, SubstrateExtrinsic, EventInfo, SubstrateEvent } from '@subsquid/hydra-common'
-import { Context } from './types';
+import { Event } from '@polkadot/types/interfaces';
+import { ExtrinsicContext, SubstrateExtrinsic, SubstrateEvent } from '@subsquid/hydra-common'
 const PREFIXES = ['0x726d726b', '0x524d524b']
-// import { encodeAddress } from "@polkadot/util-crypto";
+
 
 export type ExtraCall = {
   section: string;
@@ -15,10 +14,6 @@ export type ExtraCall = {
 export interface RemarkResult extends BaseCall {
   value: string;
   extra?: ExtraCall[];
-}
-
-export interface RemarkResultEntity extends RemarkResult {
-  id: string;
 }
 
 export interface BaseCall {
@@ -38,41 +33,9 @@ export const isUtilityBatch = (call: SubstrateExtrinsic) =>
   call.section === "utility" &&
   (call.method === "batch" || call.method === "batchAll");
 
-  export const isBatchInterrupted = (
-    records: EventRecord[],
-    extrinsicIndex?: number
-  ): boolean => {
-    const events = records.filter(
-      ({ phase, event }) =>
-        phase.isApplyExtrinsic &&
-        // phase.asApplyExtrinsic.eq(extrinsicIndex) &&
-        hasBatchFailed(event)
-    );
-  
-    return Boolean(events.length);
-  };
-
 const hasBatchFailed = (event: SubstrateEvent | Event): boolean => {
   const { method } = event;
   return method.toString() === "BatchInterrupted" || method.toString() === "ExtrinsicFailed";
-}
-
-type RemarkOrBatch = string | Vec<TCall>;
-
-export function extractRemark(processed: RemarkOrBatch, extrinsic: ExtrinsicContext): RemarkResult[] {
-  if (typeof processed === 'string' && startsWithRemark(processed)) {
-    return [toRemarkResult(processed, toBaseCall(extrinsic))]
-  }
-
-  if (Array.isArray(processed)) {
-    if (hasBatchFailed(extrinsic.event)) {
-      return [];
-    }
-
-    return processBatch(processed, toBaseCall(extrinsic));
-  }
-
-  return [];
 }
 
 export function toBaseCall(extrinsic: ExtrinsicContext): BaseCall {
@@ -91,38 +54,6 @@ export function toRemarkResult(value: string, base: BaseCall): RemarkResult {
   };
 }
 
-export const getRemarksFrom = (extrinsic: ExtrinsicContext): RemarkResult[] => {
-  if (!extrinsic.extrinsic.indexInBlock) {
-    return []
-  }
-
-  const signer = extrinsic.extrinsic.signer.toString();
-  const blockNumber = extrinsic.block.height.toString();
-  const timestamp = new Date(extrinsic.block.timestamp);
-
-  if (isSystemRemark(extrinsic.extrinsic)) {
-    return [{
-      value: extrinsic.extrinsic.args.toString(),
-      caller: signer,
-      blockNumber,
-      timestamp
-    }]
-  }
-
-  if (isUtilityBatch(extrinsic.extrinsic)) {
-    // if (isBatchInterrupted(extrinsic.block.events)) {
-    //   return [];
-    // }
-
-    console.log('is utility batch', JSON.stringify(extrinsic.extrinsic.args, null, 2))
-
-    // return processBatch(extrinsic.extrinsic.args[0] as unknown as TCall[], signer, blockNumber, timestamp)
-  }
-
-  return [];
-}
-
-
 export const processBatch = (calls: TCall[], base: BaseCall): RemarkResult[] => {
   const extra: ExtraCall[] = []
   return calls
@@ -135,4 +66,22 @@ export const processBatch = (calls: TCall[], base: BaseCall): RemarkResult[] => 
     }
   })
   .map(call => ({ value: call.args.toString(), ...base, extra }))
+}
+
+type RemarkOrBatch = string | Vec<TCall>;
+
+export function extractRemark(processed: RemarkOrBatch, extrinsic: ExtrinsicContext): RemarkResult[] {
+  if (typeof processed === 'string' && startsWithRemark(processed)) {
+    return [toRemarkResult(processed, toBaseCall(extrinsic))]
+  }
+
+  if (Array.isArray(processed)) {
+    if (hasBatchFailed(extrinsic.event)) {
+      return [];
+    }
+
+    return processBatch(processed, toBaseCall(extrinsic));
+  }
+
+  return [];
 }
