9d2ebc6de33ff0d8f8dafbc58db38a9c5399da66
Viki Val <viktorko99@gmail.com>
2021-12-14 18:21:17 +0100

    :zap: added ensure<T> function

--- src/mappings/utils/helper.ts
+++ src/mappings/utils/helper.ts
@@ -7,3 +7,7 @@ export const trimAll = (text?: string) => (text || '').replace(/\s/g, "")
 export const emoteId = ({ id, metadata }: RmrkInteraction, caller: string) => `${id}-${metadata}-${caller}`
 
 export const ensureInteraction = ({ id, metadata }: RmrkInteraction): RmrkInteraction  => ({ id: trim(id), metadata: trimAll(metadata) })
+
+export function ensure<T>(value: any): T {
+  return value as T
+}
