e40512378b4b8630958c06cf67b105a4eb1f4c06
Viki Val <viktorko99@gmail.com>
2021-12-29 10:46:55 +0100

    :zap: added pagination to the spotlight resolver

--- src/resolvers/spotlight.ts
+++ src/resolvers/spotlight.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType, Query, Resolver } from 'type-graphql'
+import { Arg, Field, ObjectType, Query, Resolver } from 'type-graphql'
 import type { EntityManager } from 'typeorm'
 import { NFTEntity } from '../generated/model'
 import { SpotlightEntity } from './model/spotlight.model'
@@ -7,13 +7,24 @@ import { SpotlightEntity } from './model/spotlight.model'
 export class SpotlightResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
+  // TODO: calculate score sold * (unique / total)
   @Query(() => [SpotlightEntity])
-  async spotlightTable(): Promise<SpotlightEntity[]> {
+  async spotlightTable(
+    @Arg('limit', { nullable: true }) limit: number,
+    @Arg('offset', { nullable: true }) offset: string,
+  ): Promise<SpotlightEntity[]> {
     const manager = await this.tx()
     const result: SpotlightEntity[] = await manager.getRepository(NFTEntity)
       .query(`
-      SELECT issuer as id, COUNT(distinct collection_id) as collections, COUNT(distinct meta_id) as unique, avg(price) as avgerage, COUNT(*) as total, COUNT(distinct current_owner) as sold from nft_entity GROUP BY issuer;
-    `)
+      SELECT
+       issuer as id, COUNT(distinct collection_id) as collections, 
+       COUNT(distinct meta_id) as unique, AVG(price) as average, 
+       COUNT(*) as total, COUNT(distinct current_owner) as sold 
+       FROM nft_entity 
+       GROUP BY issuer 
+       ORDER BY total DESC
+       LIMIT $1 OFFSET $2;
+    `, [limit, offset])
 
     return result
   }
