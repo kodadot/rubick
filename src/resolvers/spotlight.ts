2a8224183ca70cc64331f90aa56020736fe8cb82
Viki Val <viktorko99@gmail.com>
2022-01-03 14:51:40 +0100

    :card_file_box: added volume to the spotlight

--- src/resolvers/spotlight.ts
+++ src/resolvers/spotlight.ts
@@ -19,8 +19,10 @@ export class SpotlightResolver {
       SELECT
        issuer as id, COUNT(distinct collection_id) as collections, 
        COUNT(distinct meta_id) as unique, AVG(price) as average, 
-       COUNT(*) as total, COUNT(distinct current_owner) as sold 
-       FROM nft_entity 
+       COUNT(*) as total, COUNT(distinct current_owner) as sold, 
+       COALESCE(SUM(e.meta::bigint), 0) as volume 
+       FROM nft_entity ne
+       JOIN event e on e.nft_id = ne.id WHERE e.interaction = 'BUY'
        GROUP BY issuer 
        ORDER BY total DESC
        LIMIT $1 OFFSET $2;
