d38d51df4749ba4c8f7e035fe026644230d6cdf4
roiLeo <medina.leo42@gmail.com>
2022-01-06 17:08:39 +0100

    ✨ dynamic query

--- src/resolvers/series.ts
+++ src/resolvers/series.ts
@@ -31,12 +31,7 @@ export class SeriesResolver {
     @Arg('orderBy', { nullable: true, defaultValue: 'total' }) orderBy: OrderBy,
     @Arg('orderDirection', { nullable: true, defaultValue: 'DESC' }) orderDirection: OrderDirection
   ): Promise<SeriesEntity[]> {
-    const manager = await this.tx()
-    const result: SeriesEntity[] = await manager
-      .getRepository(NFTEntity)
-      .query(
-        `
-      SELECT
+    const query = `SELECT
         ce.id, ce.name, ce.meta_id as metadata, me.image, 
         COUNT(distinct ne.meta_id) as unique, 
         COUNT(distinct ne.current_owner) as unique_collectors, 
@@ -52,11 +47,12 @@ export class SeriesResolver {
       JOIN event e on ne.id = e.nft_id
       WHERE e.interaction = 'BUY'
       GROUP BY ce.id, me.image, ce.name 
-      ORDER BY $1 $2
-      LIMIT $3 OFFSET $4;
-    `,
-        [orderBy, orderDirection, limit, offset]
-      )
+      ORDER BY ${orderBy} ${orderDirection}
+      LIMIT ${limit} OFFSET ${offset}`
+    const manager = await this.tx()
+    const result: SeriesEntity[] = await manager
+      .getRepository(NFTEntity)
+      .query(query)
 
     return result
   }
