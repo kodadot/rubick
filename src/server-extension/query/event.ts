d24951791be7a979ae18931509b291c021cd0d3a
Viki Val <viktorko99@gmail.com>
2022-03-19 13:18:26 +0100

    :bug: fixed query to handle max and count buys for the collection

--- src/server-extension/query/event.ts
+++ src/server-extension/query/event.ts
@@ -1,7 +1,6 @@
 export const buyEvent = `SELECT
-    date(e.timestamp),
     COUNT(e.*) as count,
-    COALESCE(MAX(e.meta::bigint), 0) as max,
+    COALESCE(MAX(e.meta::bigint), 0) as max
     FROM event e
     LEFT JOIN nft_entity ne on ne.id = e.nft_id
     WHERE e.interaction = 'BUY' AND ne.collection_id = $1;`
