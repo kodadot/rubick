abd04c1b07ca47b7236358d3cd095250d538fa14
KoKo <kngzhi@hotmail.com>
2022-04-20 22:11:25 +0800

    fix: update db migrations

--- db/migrations/1650183454615-Data.js
+++ db/migrations/1650183454615-Data.js
@@ -2,10 +2,10 @@ module.exports = class Data1650183454615 {
   name = 'Data1650183454615'
 
   async up(db) {
-    await db.query(`ALTER TABLE "series" ADD "highest_price" numeric`)
+    await db.query(`ALTER TABLE "series" ADD "highest_sale" numeric`)
   }
 
   async down(db) {
-    await db.query(`ALTER TABLE "series" DROP COLUMN "highest_price"`)
+    await db.query(`ALTER TABLE "series" DROP COLUMN "highest_sale"`)
   }
 }
