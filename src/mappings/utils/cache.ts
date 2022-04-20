6273a85569e4562a491607c05746e0c97e9f6672
KoKo <kngzhi@hotmail.com>
2022-04-20 16:49:32 +0800

    feat: add cache and orderby for highest_sale

--- src/mappings/utils/cache.ts
+++ src/mappings/utils/cache.ts
@@ -18,6 +18,7 @@ enum Query {
             COUNT(ne.*) as total,
             AVG(ne.price) as average_price,
             MIN(NULLIF(ne.price, 0)) as floor_price,
+            COALESCE(MAX(e.meta :: bigint), 0) as highest_sale,
             COALESCE(SUM(e.meta::bigint), 0) as volume,
             COUNT(e.*) as buys
         FROM collection_entity ce
