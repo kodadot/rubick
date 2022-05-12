904797ffcd4d5810c89081706d47b52d062cd99c
Viki Val <viktorko99@gmail.com>
2022-05-12 15:29:59 +0200

    :zap: makeQuery extracts the boilerplate code from resolvers

---
+++ src/server-extension/utils.ts
@@ -0,0 +1,11 @@
+import type { EntityManager, EntityTarget, Repository } from 'typeorm'
+
+export async function makeQuery<E, V>(txManager: () => Promise<EntityManager>, entity: EntityTarget<E>, query: string, args?: any[]): Promise<V> {
+  const manager = await txManager()
+  const repository = manager.getRepository(entity)
+  return genericRepositoryQuery(repository, query, args)
+}
+
+export async function genericRepositoryQuery<T, V>(repository: Repository<T>, query: string, args?: any[]): Promise<V> {
+  return repository.query(query, args) as Promise<V>
+}
