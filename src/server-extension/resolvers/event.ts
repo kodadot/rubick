d6b16511e70540fb02da223d40859728763dd8d9
Jarsen <jarsen.dev@gmail.com>
2022-06-22 14:02:25 +0800

    last event supprot passion list

--- src/server-extension/resolvers/event.ts
+++ src/server-extension/resolvers/event.ts
@@ -3,7 +3,7 @@ import type { EntityManager } from 'typeorm'
 import { NFTEntity } from '../../model/generated'
 import { LastEventEntity } from '../model/event.model'
 import { lastEventQuery } from '../query/event'
-import { makeQuery } from "../utils";
+import { makeQuery, toSqlInParams } from "../utils";
 import { Interaction } from '../../model'
 
 @Resolver()
@@ -13,10 +13,15 @@ export class EventResolver {
   @Query(() => [LastEventEntity])
   async lastEvent(
     @Arg('interaction', { nullable: true, defaultValue: Interaction.LIST }) interaction: Interaction,
+    @Arg('passionList', () => [String!], { nullable: true }) passionList: string[],
     @Arg('limit', { nullable: true, defaultValue: 20 }) limit: number,
     @Arg('offset', { nullable: true, defaultValue: 0 }) offset: number,
   ): Promise<[LastEventEntity]> {
-    const result: [LastEventEntity] = await makeQuery(this.tx, NFTEntity, lastEventQuery, [interaction, limit, offset])
+
+    const selectFromPassionList = passionList && passionList.length > 0
+    ? `AND ne.issuer in (${toSqlInParams(passionList)})`
+    : ''
+    const result: [LastEventEntity] = await makeQuery(this.tx, NFTEntity, lastEventQuery(selectFromPassionList), [interaction, limit, offset])
     return result
   }
 
