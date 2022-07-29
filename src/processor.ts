7997bb4ed2f8afd98baa8ec24889c44f7b3a41ae
Viki Val <viktorko99@gmail.com>
2022-07-29 11:59:23 +0200

    :squid: processor loves firesquid

--- src/processor.ts
+++ src/processor.ts
@@ -1,18 +1,20 @@
+import { lookupArchive } from '@subsquid/archive-registry'
 import { SubstrateProcessor } from '@subsquid/substrate-processor'
+import { FullTypeormDatabase as Database } from '@subsquid/typeorm-store'
 import * as mappings from './mappings'
 
-const processor = new SubstrateProcessor('kusama_remark');
+const processor = new SubstrateProcessor(new Database())
 
 processor.setTypesBundle('kusama');
 processor.setBatchSize(500);
 processor.setBlockRange({from:5756453});
 processor.setDataSource({
-    archive: 'https://kusama.indexer.gc.subsquid.io/v4/graphql',
+    archive: lookupArchive("kusama", { release: "FireSquid" }),
     chain: 'wss://kusama-rpc.polkadot.io'
 });
 
-processor.addExtrinsicHandler('system.remark', mappings.handleRemark);
-processor.addExtrinsicHandler('utility.batchAll', mappings.handleBatchAll);
-processor.addExtrinsicHandler('utility.batch', mappings.handleBatch);
+processor.addCallHandler('System.remark', mappings.handleRemark);
+processor.addCallHandler('Utility.batch_all', mappings.handleBatchAll);
+processor.addCallHandler('Utility.batch', mappings.handleBatch);
 
 processor.run();
