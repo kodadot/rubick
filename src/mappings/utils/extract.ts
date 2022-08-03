62c327ec966ffad4cd35a92726bd43978657416b
Viki Val <viktorko99@gmail.com>
2022-08-03 14:05:55 +0200

    :coffin: removed batch functions

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,23 +1,17 @@
 import { SubstrateCall, SubstrateExtrinsic } from '@subsquid/substrate-processor'
-import { BalancesTransferCall } from '../../types/calls'
-import { Call, CallContext, Chain } from '../../types/support'
+import { Call } from '../../types/support'
 import { addressOf } from './helper'
 import { ArchiveCall, BaseCall, BatchArg, CallWith, Context, Transfer, UnwrapFunc } from './types'
 const PREFIXES = ['0x726d726b', '0x524d524b', 'rmrk', 'RMRK']
 
- export type ExtraCall = BatchArg
-
  export interface RemarkResult extends BaseCall {
   value: string;
-  extra?: ExtraCall[];
+  extra?: BatchArg[];
 }
 
+const startsWithRemark = (value: string, prefixes: string[] = PREFIXES): boolean => (prefixes.length < 1 || prefixes.some((word) => value.startsWith(word)))
 
-export type Records = RemarkResult[]
-
- const startsWithRemark = (value: string, prefixes: string[] = PREFIXES): boolean => (prefixes.length < 1 || prefixes.some((word) => value.startsWith(word)))
-
- const isSystemRemark = (call: ArchiveCall, prefixes: string[] = PREFIXES): boolean =>
+const isSystemRemark = (call: ArchiveCall, prefixes: string[] = PREFIXES): boolean =>
   call.__kind === 'System' &&
   call.value.__kind === "remark" &&
   startsWithRemark(call.value.remark, prefixes)
@@ -33,31 +27,12 @@ const mapToSquidCall = (arg: ArchiveCall): Call => ({
   args: arg.value
 })
 
-const mapToChainContext = (_chain: Chain) => (call: Call): CallContext => ({
-  call,
-  _chain
-})
-
 const toBalanceTransfer = (call: Call): Transfer => {
   const { dest, value } = call.args
   return { to: addressOf(dest), value }
 }
 
-const justArg = (batchArg: BatchArg): Record<string, any> => batchArg.args
-
-const valueOfArg = (args: Record<string, any>): string => args.remark || args._remark
-
-const hasBatchArgRemark = (args: Record<string, any>): boolean => {
-  const call: string = valueOfArg(args)
-  return Boolean(call) && startsWithRemark(call, PREFIXES)
-}
-
-// const hasBatchFailed = (event: SubstrateEvent | TEvent): boolean => {
-//   const { method } = event;
-//   return method.toString() === "BatchInterrupted" || method.toString() === "ExtrinsicFailed";
-// }
-
- function toBaseCall(context: Context): BaseCall {
+function toBaseCall(context: Context): BaseCall {
   const caller = addressOf(context.extrinsic.signature?.address)
   const blockNumber = context.block.height.toString();
   const timestamp = new Date(context.block.timestamp);
@@ -76,21 +51,6 @@ export function unwrap<T>(ctx: Context, unwrapFn: UnwrapFunc<T>): CallWith<T> {
     value,
     ...base
   };
-}
-
- const processBatch = (calls: BatchArg[], base: BaseCall): RemarkResult[] => {
-  const extra: ExtraCall[] = []
-  return calls
-  .map(justArg)
-  .filter((call, index) => {
-    if (hasBatchArgRemark(call)) {
-      return true
-    } else {
-      extra.push({ ...calls[index] })
-      return false
-    }
-  })
-  .map(call => ({ value: valueOfArg(call), ...base, extra }))
 }
 
 type RemarkOrBatch = string | SubstrateExtrinsic;
@@ -115,15 +75,6 @@ export function extractExtra(ctx: Context): Transfer[] {
     }
     
   }
-
-  // if (isUtilityBatch(processed)) {
-  //   if (hasBatchFailed(extrinsic.event)) {
-  //     logger.warn('[BATCH] failed', extrinsic.block.height.toString())
-  //     return [];
-  //   }
-
-  //   return processBatch(processed.args[0].value as BatchArg[], toBaseCall(extrinsic));
-  // }
 
   return [];
 }
