590796e6097282bf45a6abf152de1ab71b7f92da
Viki Val <viktorko99@gmail.com>
2022-08-03 14:36:59 +0200

    :tada: unleash the :fire::squid:

--- src/processor.ts
+++ src/processor.ts
@@ -2,9 +2,6 @@ import { lookupArchive } from '@subsquid/archive-registry'
 import { SubstrateProcessor } from '@subsquid/substrate-processor'
 import { FullTypeormDatabase as Database } from '@subsquid/typeorm-store'
 import * as mappings from './mappings'
-import { extractExtra, extractRemark } from './mappings/utils'
-import { SystemRemarkCall } from './types/calls'
-import { unwrapRemark } from '@kodadot1/minimark'
 
 const processor = new SubstrateProcessor(new Database())
 
@@ -16,20 +13,18 @@ processor.setDataSource({
     chain: 'wss://kusama-rpc.polkadot.io'
 });
 
-// processor.addCallHandler('System.remark', mappings.handleRemark);
-// processor.addCallHandler('Utility.batch_all', mappings.handleBatchAll);
-// processor.addCallHandler('Utility.batch', mappings.handleBatch);
+processor.addCallHandler('System.remark', mappings.handleRemark);
 
-processor.addCallHandler('System.remark', { range: { from: 13445851, to: 13445851 } } , async ctx => {
-    const { remark } = new SystemRemarkCall(ctx).asV1020
-    // const records = extractRemark(remark.toString(), ctx)
-    const records = unwrapRemark(remark.toString())
-    const extra = extractExtra(ctx)
-    console.log('[NAME]', ctx.extrinsic.call.name)
-    console.log('[ARGS]', extra)
-    console.log('[RMRK]', records)
-    
-    // console.log(ctx.block.height)
-});
+// DEV
+// good test values from start { from: 5756453, to: 5828351 } }
+// processor.addCallHandler('System.remark', { range: { from: 13445851, to: 13445851 } } , async ctx => {
+//     const kek = unwrap(ctx, getCreateToken)
+//     const extra = extractExtra(ctx)
+//     console.log('[NAME]', ctx.extrinsic.call.name)
+//     console.log('[EXTRA]', ctx.call.parent?.args?.calls)
+//     console.log('[EXTRA]', ctx.call.parent?.args?.calls?.at(0)?.value)
+//     console.log('[ARGS]', extra)
+//     console.log('[RMRK]', kek)
+// });
 
 processor.run();
