2a4f54b454148de4d6dd702d03d370f2ce5e9b92
Viki Val <viktorko99@gmail.com>
2022-08-03 14:07:07 +0200

    :broom: getters cleaned up

--- src/mappings/utils/getters.ts
+++ src/mappings/utils/getters.ts
@@ -1,9 +1,8 @@
-import { BalancesTransferCall, SystemRemarkCall } from '../../types/calls'
-import { Collection, Context, InteractionExtra, NFT, Transfer } from './types'
-import { CallContext } from '../../types/support'
-import { addressOf, ensureInteraction, onlyValue } from './helper'
-import { unwrapRemark, UnwrappedRemark, InteractionValue } from '@kodadot1/minimark'
+import { InteractionValue, UnwrappedRemark, unwrapRemark } from '@kodadot1/minimark'
+import { SystemRemarkCall } from '../../types/calls'
 import { extractExtra } from './extract'
+import { ensureInteraction } from './helper'
+import { Collection, Context, InteractionExtra, NFT } from './types'
 
 function getRemark<T = InteractionValue>(ctx: Context): UnwrappedRemark<T | InteractionValue> {
   const { remark } = new SystemRemarkCall(ctx).asV1020
@@ -28,27 +27,3 @@ export function getInteractionWithExtra(ctx: Context): UnwrappedRemark<Interacti
   const extra = extractExtra(ctx)
   return { ...rest, value: ensureInteraction(value), extra  }
 }
-
-// export function getBalancesTransfer(ctx: CallContext): Transfer {
-//   const transfer = new BalancesTransferCall(ctx)
-
-//   console.log(transfer.isV1020, transfer.isV1050, transfer.isV2028, transfer.isV9111)
-
-//   if (transfer.isV1020) {
-//     const { dest, value } = transfer.asV1020
-//     return { to: onlyValue(dest), value }
-//   }
-
-//   if (transfer.isV1050) {
-//     const { dest, value } =  transfer.asV1050
-//     return { to: addressOf(dest), value }
-//   }
-
-//   if (transfer.isV2028) {
-//     const { dest, value } = transfer.asV2028
-//     return { to: onlyValue(dest), value }
-//   }
-
-//   const { dest, value } = transfer.asV9111
-//   return { to: onlyValue(dest).toString('base64'), value }
-// }
