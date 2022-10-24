e591a5a3f5e3810fe7a626154eb9e73424929557
Viki Val <viktorko99@gmail.com>
2022-10-24 16:50:46 +0200

    ga

--- src/mappings/utils/metadata.ts
+++ src/mappings/utils/metadata.ts
@@ -1,69 +1,18 @@
-import Axios from 'axios'
-import { ensure } from './helper'
-import logger from './logger'
-import { SanitizerFunc, SomethingWithMeta } from './types'
 
-export const BASE_URL = 'https://kodadot.mypinata.cloud/'
+import { ensure } from '@kodadot1/metasquid'
+import { $obtain } from '@kodadot1/minipfs';
+import logger from './logger';
 
-const api = Axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: false,
-})
 
-export const sanitizeIpfsUrl = (ipfsUrl: string): string => {
-
-  const rr = /^ipfs:\/\/ipfs/
-  if (rr.test(ipfsUrl)) {
-    return ipfsUrl.replace('ipfs://', BASE_URL)
-  }
-
-  const r = /^ipfs:\/\//
-  if (r.test(ipfsUrl)) {
-    return ipfsUrl.replace('ipfs://', `${BASE_URL}ipfs/`)
-  }
-
-  return ipfsUrl
-}
-
-export const fetchMetadata = async <T>(
-  rmrk: SomethingWithMeta,
-  sanitizer: SanitizerFunc = sanitizeIpfsUrl
-): Promise<T> => {
+export const fetchMetadata = async <T>(metadata: string): Promise<T> => {
   try {
-    if (!rmrk.metadata) {
-      return ensure<T>({})
+    if (!metadata) {
+      return ensure<T>({});
     }
-
-    const { status, data } = await api.get(sanitizer(rmrk.metadata))
-    logger.watch('[IPFS]', status, rmrk.metadata)
-    if (status < 400) {
-      return data as T
-    }
+    return await $obtain<T>(metadata);
   } catch (e) {
-    logger.warn('IPFS Err', e)
+    logger.warn('IPFS Err', e);
   }
 
-  return ensure<T>({})
-}
-
-export const fetchMimeType = async (ipfsLink?: string, sanitizer: SanitizerFunc = sanitizeIpfsUrl): Promise<string | undefined> => {
-  if (!ipfsLink) {
-    return undefined
-  }
-
-  const assetUrl = sanitizer(ipfsLink)
-
-  try {
-    const { headers } = await api.head(assetUrl)
-    return headers['content-type']
-  } catch (e: any) {
-    logger.warn(`[MIME TYPE] Unable to access type of ${assetUrl}\n\nReason ${e.message}`)
-    return undefined
-  }
-}
-
-
-export default api
+  return ensure<T>({});
+};
