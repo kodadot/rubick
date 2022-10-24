8560c75d1744b8058bf8ea919f49ff308866e20b
Viki Val <viktorko99@gmail.com>
2022-10-24 18:17:16 +0200

    :truck: moved list into shared

---
+++ src/mappings/shared/list.ts
@@ -0,0 +1,38 @@
+import { ensure } from '@kodadot1/metasquid'
+import { get } from '@kodadot1/metasquid/entity'
+import { Optional } from '@kodadot1/metasquid/types'
+
+import { NFTEntity } from '../../model'
+import { unwrap } from '../utils'
+import { isOwnerOrElseError, isPositiveOrElseError, validateInteraction } from '../utils/consolidator'
+import { getInteraction } from '../utils/getters'
+import logger, { logError } from '../utils/logger'
+import { Context, RmrkEvent, RmrkInteraction } from '../utils/types'
+import { createEvent } from './create'
+
+export async function list(context: Context) {
+  let interaction: Optional<RmrkInteraction> = null
+
+  try {
+    const { value, caller, timestamp, blockNumber } = unwrap(context, getInteraction);
+    interaction = value
+    const nft = ensure<NFTEntity>(
+      await get<NFTEntity>(context.store, NFTEntity, interaction.id)
+    )
+    validateInteraction(nft, interaction)
+    isOwnerOrElseError(nft, caller)
+    const price = BigInt(interaction.value || '0')
+    isPositiveOrElseError(price)
+    nft.price = price
+    nft.updatedAt = timestamp
+
+    logger.success(`[LIST] ${nft.id} from ${caller}`)
+    await context.store.save(nft)
+    const event = nft.price === 0n ? RmrkEvent.UNLIST : RmrkEvent.LIST
+    await createEvent(nft, event, { blockNumber, caller, timestamp }, String(price), context.store)
+  } catch (e) {
+    logError(e, (e) =>
+      logger.warn(`[LIST] ${e.message} ${JSON.stringify(interaction)}`)
+    )
+  }
+}
