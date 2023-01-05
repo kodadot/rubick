8362e62ae05a17d4abd154f7b1f8de4f31d1a0c9
Viki Val <viktorko99@gmail.com>
2023-01-05 16:09:32 +0100

    :squid: :soap: does new fomating for models

--- src/model/generated/collectionEntity.model.ts
+++ src/model/generated/collectionEntity.model.ts
@@ -6,57 +6,57 @@ import {MetadataEntity} from "./metadataEntity.model"
 
 @Entity_()
 export class CollectionEntity {
-  constructor(props?: Partial<CollectionEntity>) {
-    Object.assign(this, props)
-  }
+    constructor(props?: Partial<CollectionEntity>) {
+        Object.assign(this, props)
+    }
 
-  @Column_("text", {nullable: true})
-  version!: string | undefined | null
+    @Column_("text", {nullable: true})
+    version!: string | undefined | null
 
-  @Index_()
-  @Column_("text", {nullable: true})
-  name!: string | undefined | null
+    @Index_()
+    @Column_("text", {nullable: true})
+    name!: string | undefined | null
 
-  @Column_("int4", {nullable: false})
-  max!: number
+    @Column_("int4", {nullable: false})
+    max!: number
 
-  @Column_("text", {nullable: true})
-  issuer!: string | undefined | null
+    @Column_("text", {nullable: true})
+    issuer!: string | undefined | null
 
-  @Column_("text", {nullable: true})
-  symbol!: string | undefined | null
+    @Column_("text", {nullable: true})
+    symbol!: string | undefined | null
 
-  @PrimaryColumn_()
-  id!: string
+    @PrimaryColumn_()
+    id!: string
 
-  @Column_("text", {nullable: true})
-  metadata!: string | undefined | null
+    @Column_("text", {nullable: true})
+    metadata!: string | undefined | null
 
-  @Column_("text", {nullable: true})
-  currentOwner!: string | undefined | null
+    @Column_("text", {nullable: true})
+    currentOwner!: string | undefined | null
 
-  @OneToMany_(() => NFTEntity, e => e.collection)
-  nfts!: NFTEntity[]
+    @OneToMany_(() => NFTEntity, e => e.collection)
+    nfts!: NFTEntity[]
 
-  @Column_("jsonb", {transformer: {to: obj => obj == null ? undefined : obj.map((val: any) => val.toJSON()), from: obj => obj == null ? undefined : marshal.fromList(obj, val => new CollectionEvent(undefined, marshal.nonNull(val)))}, nullable: true})
-  events!: (CollectionEvent)[] | undefined | null
+    @Column_("jsonb", {transformer: {to: obj => obj == null ? undefined : obj.map((val: any) => val.toJSON()), from: obj => obj == null ? undefined : marshal.fromList(obj, val => new CollectionEvent(undefined, marshal.nonNull(val)))}, nullable: true})
+    events!: (CollectionEvent)[] | undefined | null
 
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
-  blockNumber!: bigint | undefined | null
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
+    blockNumber!: bigint | undefined | null
 
-  @Index_()
-  @ManyToOne_(() => MetadataEntity, {nullable: true})
-  meta!: MetadataEntity | undefined | null
+    @Index_()
+    @ManyToOne_(() => MetadataEntity, {nullable: true})
+    meta!: MetadataEntity | undefined | null
 
-  @Column_("timestamp with time zone", {nullable: false})
-  createdAt!: Date
+    @Column_("timestamp with time zone", {nullable: false})
+    createdAt!: Date
 
-  @Column_("timestamp with time zone", {nullable: false})
-  updatedAt!: Date
+    @Column_("timestamp with time zone", {nullable: false})
+    updatedAt!: Date
 
-  @Column_("int4", {nullable: false})
-  nftCount!: number
+    @Column_("int4", {nullable: false})
+    nftCount!: number
 
-  @Column_("int4", {nullable: false})
-  supply!: number
+    @Column_("int4", {nullable: false})
+    supply!: number
 }
