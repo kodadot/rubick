1ec975ef2c4e4e6df32fd2f3d670b12e69b54995
Viki Val <viktorko99@gmail.com>
2023-01-19 19:31:59 +0100

    :art: use interaction from v2 instead of action

--- src/mappings/v2/index.ts
+++ src/mappings/v2/index.ts
@@ -1,4 +1,4 @@
-import { unwrapRemarkV2 as unwrapRemark } from '@vikiival/minimark/v2'
+import { unwrapRemarkV2 as unwrapRemark, Interaction } from '@vikiival/minimark/v2'
 import { createCollection } from '../shared/create'
 import { unwrap } from '../utils/extract'
 import logger from '../utils/logger'
@@ -8,39 +8,70 @@ export async function mainFrame(remark: string, context: Context): Promise<void>
   const base = unwrap(context, (_: Context) => ({ value: remark }))
   try {
     const { interaction: event, version } = unwrapRemark<RmrkInteraction<true>>(remark.toString())
-    logger.pending(`[${event === Action.CREATE ? 'COLLECTION' : event}]: ${base.blockNumber}`)
+    logger.pending(`[${event === Interaction.CREATE ? 'COLLECTION' : event}]: ${base.blockNumber}`)
 
     switch (event) {
-      case Action.CREATE: // should be CRATE
+      case Interaction.CREATE: // should be CRATE
         await createCollection(context)
         break
-      case Action.MINT:
+      case Interaction.MINT:
         logger.info(`[MINT]::${base.blockNumber}`)
         // await mintNFT(context)
         break
-      case Action.SEND:
+      case Interaction.SEND:
         logger.info(`[SEND]::${base.blockNumber}`)
         // await send(context)
         break
-      case Action.BUY:
+      case Interaction.BUY:
         logger.info(`[BUY]::${base.blockNumber}`)
         // await buy(context)
         break
-      case Action.BURN:
+      case Interaction.BURN:
         logger.info(`[BURN]::${base.blockNumber}`)
         // await consume(context)
         break
-      case Action.LIST:
+      case Interaction.LIST:
         logger.info(`[LIST]::${base.blockNumber}`)
         // await list(context)
         break
-      case Action.CHANGEISSUER:
+      case Interaction.CHANGEISSUER:
         logger.info(`[CHANGEISSUER]::${base.blockNumber}`)
         // await changeIssuer(context)
         break
-      case Action.EMOTE:
+      case Interaction.EMOTE:
         logger.info(`[EMOTE]::${base.blockNumber}`)
         // await emote(context)
+        break
+      // RMRK v2.0.0
+      case Interaction.ACCEPT:
+        logger.info(`[ACCEPT]::${base.blockNumber}`)
+        break
+      case Interaction.BASE:
+        logger.info(`[BASE]::${base.blockNumber}`)
+        break
+      case Interaction.DESTROY:
+        logger.info(`[DESTROY]::${base.blockNumber}`)
+        break
+      case Interaction.SETPRIORITY:
+        logger.info(`[SETPRIORITY]::${base.blockNumber}`)
+        break
+      case Interaction.SETPROPERTY:
+        logger.info(`[SETPROPERTY]::${base.blockNumber}`)
+        break
+      case Interaction.THEMEADD:
+        logger.info(`[THEMEADD]::${base.blockNumber}`)
+        break
+      case Interaction.RESADD:
+        logger.info(`[RESADD]::${base.blockNumber}`)
+        break
+      case Interaction.EQUIP:
+        logger.info(`[EQUIP]::${base.blockNumber}`)
+        break
+      case Interaction.EQUIPPABLE:
+        logger.info(`[EQUIPPABLE]::${base.blockNumber}`)
+        break
+      case Interaction.LOCK:
+        logger.info(`[LOCK]::${base.blockNumber}`)
         break
       default:
         logger.start(
