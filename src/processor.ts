01cfb9d1e6693d403d9587fb0fcf2a4e594953e5
Viki Val <viktorko99@gmail.com>
2023-01-19 18:56:13 +0100

    :beers: Testing smoll RMRKv2 subset

--- src/processor.ts
+++ src/processor.ts
@@ -5,9 +5,13 @@ import * as mappings from './mappings'
 
 const processor = new SubstrateProcessor(new Database())
 
+const STARTING_BLOCK = 16260068;
+const ENDING_BLOCK = 16261119;
+
 processor.setTypesBundle('kusama');
 processor.setBatchSize(30);
-processor.setBlockRange({from:5756453});
+// processor.setBlockRange({ from: 5756453 });
+processor.setBlockRange({ from: STARTING_BLOCK, to: ENDING_BLOCK });
 processor.setDataSource({
     archive: lookupArchive("kusama", { release: "FireSquid" }),
     chain: 'wss://kusama-rpc.polkadot.io'
