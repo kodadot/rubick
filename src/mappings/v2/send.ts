597e5759bc124a520849cebb989b36369957f1c0
Viki Val <viktorko99@gmail.com>
2023-02-09 16:42:58 +0100

    :zap: Wish that it works

--- src/mappings/v2/send.ts
+++ src/mappings/v2/send.ts
@@ -1,17 +1,15 @@
-import { ensure } from '@kodadot1/metasquid'
-import { getOrFail as get, findByRawQuery } from '@kodadot1/metasquid/entity'
+import { getOrFail as get } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
 
 import { NFTEntity } from '../../model'
+import { createEvent } from '../shared/event'
 import { unwrap } from '../utils'
 import { isOwnerOrElseError, validateInteraction } from '../utils/consolidator'
+import { findRootItemById } from '../utils/entity'
 import { getInteraction } from '../utils/getters'
-import logger, { logError } from '../utils/logger'
-import { Context, Action, RmrkInteraction } from '../utils/types'
-import { createEvent } from '../shared/event'
 import { isDummyAddress } from '../utils/helper'
-import { plsBe, real } from '@kodadot1/metasquid/consolidator'
-import { rootOwnerQuery } from '../../server-extension/query/nft'
+import logger, { logError } from '../utils/logger'
+import { Action, Context, RmrkInteraction } from '../utils/types'
 
 export async function send(context: Context) {
   let interaction: Optional<RmrkInteraction> = null
@@ -26,13 +24,8 @@ export async function send(context: Context) {
     const originalOwner = nft.currentOwner ?? undefined
     const recipient = interaction.value || ''
 
-    const rootNFT = ensure<NFTEntity>(await findByRawQuery(context.store, NFTEntity, rootOwnerQuery, [interaction.id]).then((res) => res.at(0)))
-    plsBe(real, rootNFT)
-    isOwnerOrElseError(rootNFT, caller)
-
     const isRecipientNFT = !isDummyAddress(recipient)
 
-    nft.currentOwner = recipient
     nft.price = BigInt(0)
     nft.updatedAt = timestamp
 
@@ -40,12 +33,15 @@ export async function send(context: Context) {
     if (isRecipientNFT) {
       const parent = await get<NFTEntity>(context.store, NFTEntity, recipient)
       const isCallerTheOwner = parent.currentOwner === caller
-      const rootRecipientNFT = ensure<NFTEntity>(await findByRawQuery(context.store, NFTEntity, rootOwnerQuery, [recipient]).then((res) => res.at(0)))
-      plsBe(real, rootRecipientNFT)
+      const rootRecipientNFT = await findRootItemById(context.store, recipient)
 
       nft.currentOwner = rootRecipientNFT.currentOwner
-      // nft.pending = !isCallerTheOwner;
+      nft.pending = !isCallerTheOwner;
       nft.parent = parent
+    } else {
+      nft.parent = null
+      nft.currentOwner = recipient
+      nft.pending = false
     }
 
     logger.success(`[SEND] ${nft.id} to ${interaction.value}`)
