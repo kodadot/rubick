058cfff130ffb4e82665259be88da42072413171
Viki Val <viktorko99@gmail.com>
2023-02-09 17:05:18 +0100

    :zap: accept

--- src/mappings/v2/accept.ts
+++ src/mappings/v2/accept.ts
@@ -1,10 +1,10 @@
 import { ensure } from '@kodadot1/metasquid'
 import { burned, plsBe, plsNotBe, real } from '@kodadot1/metasquid/consolidator'
-import { get } from '@kodadot1/metasquid/entity'
+import { getOrFail as get } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
 import { Accept } from '@vikiival/minimark/v2'
 
-import { NFTEntity } from '../../model'
+import { NFTEntity, Resource } from '../../model'
 import { createEvent } from '../shared/event'
 import { unwrap } from '../utils'
 import { isOwnerOrElseError } from '../utils/consolidator'
@@ -19,23 +19,35 @@ export async function acceptResource(context: Context) {
 
   try {
     const { value: interaction, caller, timestamp, blockNumber, version } = unwrap(context, getAcceptResource);
-    const nft = ensure<NFTEntity>(
-      await get<NFTEntity>(context.store, NFTEntity, interaction.id)
-    )
-    plsBe(real, nft)
+    const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
     plsNotBe(burned, nft)
     isOwnerOrElseError(nft, caller)
-    nft.updatedAt = timestamp
+    
 
-    // TODO: add logic for accepting resource
+    switch (interaction.entity_type) {
+      case 'NFT':
+        const pendingNFT = await get<NFTEntity>(context.store, NFTEntity, interaction.entity_id)
+        plsNotBe(burned, nft)
+        pendingNFT.updatedAt = timestamp
+        pendingNFT.pending = false
+        await context.store.save(nft)
+        break;
+      case 'RES':
+        const res = await get<Resource>(context.store, Resource, interaction.entity_id)
+        res.pending = false
+        await context.store.save(res)
+        break;
+      default:
+        throw new Error(`Unknown entity type ${interaction.entity_type}`)
+    }
 
-    logger.success(`[ACCEPT] ${nft.id} from ${caller}`)
+    logger.success(`[${OPERATION}] ${interaction.entity_type}::${interaction.entity_id} in ${nft.id} from ${caller}`)
     await context.store.save(nft)
     await createEvent(nft, Action.ACCEPT, { blockNumber, caller, timestamp, version }, `${interaction.entity_type}::${interaction.entity_id}`, context.store)
 
   } catch (e) {
     logError(e, (e) =>
-      logger.warn(`[ACCEPT] ${e.message} ${JSON.stringify(interaction)}`)
+      logger.warn(`[${OPERATION}] ${e.message} ${JSON.stringify(interaction)}`)
     )
   }
 }
