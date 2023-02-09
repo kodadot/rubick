2f4377de6563ac9d3f8c54a05847ee571b5845be
Viki Val <viktorko99@gmail.com>
2023-02-09 20:39:58 +0100

    :zap: addResource??

--- src/mappings/v2/addResource.ts
+++ src/mappings/v2/addResource.ts
@@ -1,10 +1,10 @@
 import { ensure } from '@kodadot1/metasquid'
 import { burned, plsBe, plsNotBe, real } from '@kodadot1/metasquid/consolidator'
-import { get } from '@kodadot1/metasquid/entity'
+import { getOrCreate, getOrFail as get } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
 import { Resadd } from '@vikiival/minimark/v2'
 
-import { NFTEntity } from '../../model'
+import { NFTEntity, Resource } from '../../model'
 import { handleMetadata } from '../shared'
 import { createEvent } from '../shared/event'
 import { unwrap } from '../utils'
@@ -21,24 +21,29 @@ export async function addResource(context: Context) {
   try {
     const getter = getAddRes
     const { value: interaction, caller, timestamp, blockNumber, version } = unwrap(context, getter);
-    const nft = ensure<NFTEntity>(
-      await get<NFTEntity>(context.store, NFTEntity, interaction.id)
-    )
-    plsBe(real, nft)
+    const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
     plsNotBe(burned, nft)
     isIssuerOrElseError(nft, caller)
     const isPending = nft.currentOwner !== caller
     nft.updatedAt = timestamp
 
+    const final = await getOrCreate(context.store, Resource, interaction.value.id, { ...interaction.value,  })
+
     if (interaction.value.metadata) {
       const metadata = await handleMetadata(interaction.value.metadata, '', context.store)
       logger.log(`[${OPERATION}] ${nft.id} metadata ${metadata?.id}`)
+      final.meta = metadata
     }
 
-    // TODO: add logic for accepting resource
+    final.pending = isPending
+    final.nft = nft
+    final.metadata = interaction.value.metadata
+    final.src = interaction.value.src
+    final.thumb = interaction.value.thumb
 
     logger.success(`[${OPERATION}] ${nft.id} from ${caller}`)
     await context.store.save(nft)
+    await context.store.save(final)
     await createEvent(nft, OPERATION, { blockNumber, caller, timestamp, version }, `${interaction.value.id}`, context.store)
 
   } catch (e) {
