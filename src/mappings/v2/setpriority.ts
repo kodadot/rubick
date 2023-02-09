bad8f5c3ad37bfed41496f4bc7ff2b6ecefc73bd
Viki Val <viktorko99@gmail.com>
2023-02-09 21:14:36 +0100

    :zap: setPriority??

--- src/mappings/v2/setpriority.ts
+++ src/mappings/v2/setpriority.ts
@@ -1,10 +1,11 @@
 import { ensure } from '@kodadot1/metasquid'
 import { burned, plsBe, plsNotBe, real } from '@kodadot1/metasquid/consolidator'
-import { get } from '@kodadot1/metasquid/entity'
+import { findByIdList, getOrFail as get } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
+import { assertNotNull } from '@subsquid/substrate-processor'
 import { Resadd, SetPriority } from '@vikiival/minimark/v2'
 
-import { NFTEntity } from '../../model'
+import { NFTEntity, Resource } from '../../model'
 import { handleMetadata } from '../shared'
 import { createEvent } from '../shared/event'
 import { unwrap } from '../utils'
@@ -21,9 +22,7 @@ export async function setPriority(context: Context) {
   try {
     const getter = getSetPriority
     const { value: interaction, caller, timestamp, blockNumber, version } = unwrap(context, getter);
-    const nft = ensure<NFTEntity>(
-      await get<NFTEntity>(context.store, NFTEntity, interaction.id)
-    )
+    const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
     plsBe(real, nft)
     plsNotBe(burned, nft)
     isOwnerOrElseError(nft, caller)
@@ -31,10 +30,20 @@ export async function setPriority(context: Context) {
 
     logger.success(`[${OPERATION}] NEW PRIORITY ${interaction.value} for ${nft.id} from ${caller}`)
 
-    // TODO: add logic for accepting resource
+    const resourceList = await findByIdList(context.store, Resource, interaction.value)
+    
+    if (resourceList.length !== interaction.value.length) {
+      throw new Error(`[${OPERATION}] Resource list length mismatch need ${resourceList.length} got ${interaction.value.length}`)
+    }
 
-    logger.success(`[${OPERATION}] ${nft.id} from ${caller}`)
+    resourceList.forEach((resource) => {
+      resource.priority = interaction.value.indexOf(resource.id)
+    })
+
+  
+    await context.store.save(resourceList)
     await context.store.save(nft)
+    logger.success(`[${OPERATION}] ${nft.id} from ${caller}`)
     await createEvent(nft, OPERATION, { blockNumber, caller, timestamp, version }, `${interaction.value.at(0)}`, context.store)
 
   } catch (e) {
