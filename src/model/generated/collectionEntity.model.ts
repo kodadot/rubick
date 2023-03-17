887badbe268c776e6bc03f3391d46daf1a664348
Viki Val <viktorko99@gmail.com>
2023-03-17 14:14:50 +0100

    🎉 flat image and media

--- src/model/generated/collectionEntity.model.ts
+++ src/model/generated/collectionEntity.model.ts
@@ -1,8 +1,8 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_, OneToMany as OneToMany_, ManyToOne as ManyToOne_} from "typeorm"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_, OneToMany as OneToMany_} from "typeorm"
 import * as marshal from "./marshal"
-import {NFTEntity} from "./nftEntity.model"
 import {CollectionEvent} from "./_collectionEvent"
 import {MetadataEntity} from "./metadataEntity.model"
+import {NFTEntity} from "./nftEntity.model"
 
 @Entity_()
 export class CollectionEntity {
@@ -10,53 +10,59 @@ export class CollectionEntity {
         Object.assign(this, props)
     }
 
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
+    blockNumber!: bigint | undefined | null
+
+    @Column_("timestamp with time zone", {nullable: false})
+    createdAt!: Date
+
     @Column_("text", {nullable: true})
-    version!: string | undefined | null
+    currentOwner!: string | undefined | null
+
+    @Column_("jsonb", {transformer: {to: obj => obj == null ? undefined : obj.map((val: any) => val.toJSON()), from: obj => obj == null ? undefined : marshal.fromList(obj, val => new CollectionEvent(undefined, marshal.nonNull(val)))}, nullable: true})
+    events!: (CollectionEvent)[] | undefined | null
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Column_("text", {nullable: true})
+    image!: string | undefined | null
+
+    @Column_("text", {nullable: true})
+    issuer!: string | undefined | null
+
+    @Column_("int4", {nullable: false})
+    max!: number
+
+    @Column_("text", {nullable: true})
+    media!: string | undefined | null
+
+    @Index_()
+    @ManyToOne_(() => MetadataEntity, {nullable: true})
+    meta!: MetadataEntity | undefined | null
+
+    @Column_("text", {nullable: true})
+    metadata!: string | undefined | null
 
     @Index_()
     @Column_("text", {nullable: true})
     name!: string | undefined | null
 
     @Column_("int4", {nullable: false})
-    max!: number
-
-    @Column_("text", {nullable: true})
-    issuer!: string | undefined | null
-
-    @Column_("text", {nullable: true})
-    symbol!: string | undefined | null
-
-    @PrimaryColumn_()
-    id!: string
-
-    @Column_("text", {nullable: true})
-    metadata!: string | undefined | null
-
-    @Column_("text", {nullable: true})
-    currentOwner!: string | undefined | null
+    nftCount!: number
 
     @OneToMany_(() => NFTEntity, e => e.collection)
     nfts!: NFTEntity[]
 
-    @Column_("jsonb", {transformer: {to: obj => obj == null ? undefined : obj.map((val: any) => val.toJSON()), from: obj => obj == null ? undefined : marshal.fromList(obj, val => new CollectionEvent(undefined, marshal.nonNull(val)))}, nullable: true})
-    events!: (CollectionEvent)[] | undefined | null
+    @Column_("int4", {nullable: false})
+    supply!: number
 
-    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
-    blockNumber!: bigint | undefined | null
-
-    @Index_()
-    @ManyToOne_(() => MetadataEntity, {nullable: true})
-    meta!: MetadataEntity | undefined | null
-
-    @Column_("timestamp with time zone", {nullable: false})
-    createdAt!: Date
+    @Column_("text", {nullable: true})
+    symbol!: string | undefined | null
 
     @Column_("timestamp with time zone", {nullable: false})
     updatedAt!: Date
 
-    @Column_("int4", {nullable: false})
-    nftCount!: number
-
-    @Column_("int4", {nullable: false})
-    supply!: number
+    @Column_("text", {nullable: true})
+    version!: string | undefined | null
 }
--- src/model/generated/nftEntity.model.ts
+++ src/model/generated/nftEntity.model.ts
@@ -1,8 +1,8 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_, ManyToOne as ManyToOne_, OneToMany as OneToMany_} from "typeorm"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_, OneToMany as OneToMany_} from "typeorm"
 import * as marshal from "./marshal"
 import {CollectionEntity} from "./collectionEntity.model"
-import {Event} from "./event.model"
 import {Emote} from "./emote.model"
+import {Event} from "./event.model"
 import {MetadataEntity} from "./metadataEntity.model"
 
 @Entity_()
@@ -11,65 +11,71 @@ export class NFTEntity {
         Object.assign(this, props)
     }
 
-    @Index_()
-    @Column_("text", {nullable: true})
-    name!: string | undefined | null
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
+    blockNumber!: bigint | undefined | null
 
-    @Column_("text", {nullable: true})
-    instance!: string | undefined | null
-
-    @Column_("int4", {nullable: true})
-    transferable!: number | undefined | null
+    @Column_("bool", {nullable: false})
+    burned!: boolean
 
     @Index_()
     @ManyToOne_(() => CollectionEntity, {nullable: true})
     collection!: CollectionEntity
 
-    @Column_("text", {nullable: true})
-    issuer!: string | undefined | null
+    @Column_("timestamp with time zone", {nullable: false})
+    createdAt!: Date
 
+    @Index_()
     @Column_("text", {nullable: true})
-    sn!: string | undefined | null
+    currentOwner!: string | undefined | null
 
-    @PrimaryColumn_()
-    id!: string
+    @Column_("int4", {nullable: false})
+    emoteCount!: number
+
+    @OneToMany_(() => Emote, e => e.nft)
+    emotes!: Emote[]
+
+    @OneToMany_(() => Event, e => e.nft)
+    events!: Event[]
 
     @Index_()
     @Column_("text", {nullable: false})
     hash!: string
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Column_("text", {nullable: true})
+    instance!: string | undefined | null
+
+    @Column_("text", {nullable: true})
+    image!: string | undefined | null
+
+    @Column_("text", {nullable: true})
+    issuer!: string | undefined | null
+
+    @Column_("text", {nullable: true})
+    media!: string | undefined | null
+
+    @Index_()
+    @ManyToOne_(() => MetadataEntity, {nullable: true})
+    meta!: MetadataEntity | undefined | null
 
     @Column_("text", {nullable: true})
     metadata!: string | undefined | null
 
     @Index_()
     @Column_("text", {nullable: true})
-    currentOwner!: string | undefined | null
+    name!: string | undefined | null
 
     @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
     price!: bigint
 
-    @Column_("bool", {nullable: false})
-    burned!: boolean
+    @Column_("text", {nullable: true})
+    sn!: string | undefined | null
 
-    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
-    blockNumber!: bigint | undefined | null
-
-    @OneToMany_(() => Event, e => e.nft)
-    events!: Event[]
-
-    @OneToMany_(() => Emote, e => e.nft)
-    emotes!: Emote[]
-
-    @Index_()
-    @ManyToOne_(() => MetadataEntity, {nullable: true})
-    meta!: MetadataEntity | undefined | null
-
-    @Column_("timestamp with time zone", {nullable: false})
-    createdAt!: Date
+    @Column_("int4", {nullable: true})
+    transferable!: number | undefined | null
 
     @Column_("timestamp with time zone", {nullable: false})
     updatedAt!: Date
-
-    @Column_("int4", {nullable: false})
-    emoteCount!: number
 }
