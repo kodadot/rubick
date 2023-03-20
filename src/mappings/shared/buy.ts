d362009beaf3f65c5bcbc561362c9ce2932ecc97
Viki Val <viktorko99@gmail.com>
2023-03-20 15:03:04 +0100

    :bug: non-existing joins :)

--- src/mappings/shared/buy.ts
+++ src/mappings/shared/buy.ts
@@ -1,14 +1,13 @@
-import { ensure } from '@kodadot1/metasquid'
-import { plsBe, real } from '@kodadot1/metasquid/consolidator'
-import { getOrFail as get } from '@kodadot1/metasquid/entity'
+import { serializer } from '@kodadot1/metasquid'
+import { getWith } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
 
-import { CollectionEntity, NFTEntity } from '../../model'
+import { NFTEntity } from '../../model'
 import { unwrap } from '../utils'
 import { isBuyLegalOrElseError, isInteractive, isPositiveOrElseError } from '../utils/consolidator'
 import { getInteractionWithExtra } from '../utils/getters'
 import logger, { logError } from '../utils/logger'
-import { Context, Action, RmrkInteraction } from '../utils/types'
+import { Action, Context, RmrkInteraction } from '../utils/types'
 import { createEvent } from './event'
 
 export async function buy(context: Context) {
@@ -17,7 +16,8 @@ export async function buy(context: Context) {
   try {
     const { value, caller, timestamp, blockNumber, extra } = unwrap(context, getInteractionWithExtra);
     interaction = value
-    const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
+    const nft = await getWith<NFTEntity>(context.store, NFTEntity, interaction.id, { collection: true })
+    logger.debug(`[BUY] ${nft.id} from ${caller}, ${JSON.stringify(nft, serializer, 2)}`)
     isInteractive(nft)
     isPositiveOrElseError(nft.price, true)
     isBuyLegalOrElseError(nft, extra || [])
@@ -27,13 +27,10 @@ export async function buy(context: Context) {
     nft.price = BigInt(0)
     nft.updatedAt = timestamp
 
-    plsBe(real, nft.collection)
-    const collection = await get<CollectionEntity>(context.store, CollectionEntity, nft.collection.toString())
-    collection.updatedAt = timestamp
+    nft.collection.updatedAt = timestamp
 
     logger.success(`[BUY] ${nft.id} from ${caller}`)
     await context.store.save(nft)
-    await context.store.save(collection)
     await createEvent(nft, Action.BUY, { blockNumber, caller, timestamp }, String(originalPrice), context.store, originalOwner)
   } catch (e) {
     logError(e, (e) =>
