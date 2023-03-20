1bbaa6f8c44d1f9655b7065706e9a742c33541a5
Viki Val <viktorko99@gmail.com>
2023-03-20 15:35:31 +0100

    :construction: use metaquid logger instead of signale

--- src/mappings/utils/logger.ts
+++ src/mappings/utils/logger.ts
@@ -1,4 +1,6 @@
-import singale from 'signale'
+import { serializer } from '@kodadot1/metasquid'
+import { logger } from '@kodadot1/metasquid/logger'
+import { Action } from '../utils/types'
 
 type ErrorCallback = (error: Error) => void;
 
@@ -8,4 +10,16 @@ export const logError = (e: Error | unknown, cb: ErrorCallback) => {
   }
 }
 
-export default singale
+export const success = (action: Action, message: string) => {
+  logger.info(`[${action}] ${message}`)
+}
+
+export const error = (e: Error, action: Action, message: string) => {
+  logError(e, (e) => logger.error(`[${action}] ${e.message} ${message}`))
+}
+
+export const debug = (action: Action, message: Record<any, any>, serialize?: boolean) => {
+  logger.debug(`[${action}] ${JSON.stringify(message, serialize ?  serializer : undefined, 2)}`)
+}
+
+export default logger
