eaf749996e71043b9dc3f0e6fa8863e6395553e2
Viki Val <viktorko99@gmail.com>
2023-03-20 15:57:49 +0100

    :loud_sound: in src/mappings/shared/buy.ts

--- src/mappings/shared/buy.ts
+++ src/mappings/shared/buy.ts
@@ -1,4 +1,3 @@
-import { serializer } from '@kodadot1/metasquid'
 import { getWith } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
 
@@ -6,9 +5,11 @@ import { NFTEntity } from '../../model'
 import { unwrap } from '../utils'
 import { isBuyLegalOrElseError, isInteractive, isPositiveOrElseError } from '../utils/consolidator'
 import { getInteractionWithExtra } from '../utils/getters'
-import logger, { logError } from '../utils/logger'
+import { error, success } from '../utils/logger'
 import { Action, Context, RmrkInteraction } from '../utils/types'
 import { createEvent } from './event'
+
+const OPERATION = Action.BUY
 
 export async function buy(context: Context) {
   let interaction: Optional<RmrkInteraction> = null
@@ -17,7 +18,6 @@ export async function buy(context: Context) {
     const { value, caller, timestamp, blockNumber, extra } = unwrap(context, getInteractionWithExtra);
     interaction = value
     const nft = await getWith<NFTEntity>(context.store, NFTEntity, interaction.id, { collection: true })
-    logger.debug(`[BUY] ${nft.id} from ${caller}, ${JSON.stringify(nft, serializer, 2)}`)
     isInteractive(nft)
     isPositiveOrElseError(nft.price, true)
     isBuyLegalOrElseError(nft, extra || [])
@@ -29,12 +29,10 @@ export async function buy(context: Context) {
 
     nft.collection.updatedAt = timestamp
 
-    logger.success(`[BUY] ${nft.id} from ${caller}`)
+    success(OPERATION, `${nft.id} from ${caller}`)
     await context.store.save(nft)
-    await createEvent(nft, Action.BUY, { blockNumber, caller, timestamp }, String(originalPrice), context.store, originalOwner)
+    await createEvent(nft, OPERATION, { blockNumber, caller, timestamp }, String(originalPrice), context.store, originalOwner)
   } catch (e) {
-    logError(e, (e) =>
-      logger.error(`[BUY] ${e.message} ${JSON.stringify(interaction)}`)
-    )
+    error(e, OPERATION, JSON.stringify(interaction))
   }
 }
