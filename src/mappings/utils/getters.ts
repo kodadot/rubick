f21d187f71438b1f32cab4e228345704fed18606
Viki Val <viktorko99@gmail.com>
2023-03-20 15:11:08 +0100

    :zap: getRemarkString

--- src/mappings/utils/getters.ts
+++ src/mappings/utils/getters.ts
@@ -4,9 +4,14 @@ import { extractExtra } from './extract'
 import { ensureInteraction } from './helper'
 import { Collection, Context, InteractionExtra, NFT } from './types'
 
-export function getRemark<T = InteractionValue>(ctx: Context): UnwrappedRemark<T | InteractionValue> {
+export function getRemarkString(ctx: Context): string {
   const { remark } = new SystemRemarkCall(ctx).asV1020
-  return unwrapRemark<T>(remark.toString())
+  return remark.toString()
+}
+
+export function getRemark<T = InteractionValue>(ctx: Context): UnwrappedRemark<T | InteractionValue> {
+  const remark  = getRemarkString(ctx)
+  return unwrapRemark<T>(remark)
 }
 
 export function getCreateCollection(ctx: Context): UnwrappedRemark<Collection> {
