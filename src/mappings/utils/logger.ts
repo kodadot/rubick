d7c4792b8d223f9d03f7ff2164ab00e28c62a6ff
Viki Val <viktorko99@gmail.com>
2023-03-20 15:56:06 +0100

    :zap: new handy logging fns

--- src/mappings/utils/logger.ts
+++ src/mappings/utils/logger.ts
@@ -1,6 +1,9 @@
 import { serializer } from '@kodadot1/metasquid'
 import { logger } from '@kodadot1/metasquid/logger'
-import { Action } from '../utils/types'
+import { Action as MetaAction  } from '../utils/types'
+import { Interaction } from '@vikiival/minimark/v1'
+
+type Action = MetaAction | Interaction
 
 type ErrorCallback = (error: Error) => void;
 
@@ -11,11 +14,19 @@ export const logError = (e: Error | unknown, cb: ErrorCallback) => {
 }
 
 export const success = (action: Action, message: string) => {
-  logger.info(`[${action}] ${message}`)
+  logger.info(`💚 [${action}] ${message}`)
 }
 
-export const error = (e: Error, action: Action, message: string) => {
-  logError(e, (e) => logger.error(`[${action}] ${e.message} ${message}`))
+export const error = (e: Error | unknown, action: Action, message: string) => {
+  logError(e, (e) => logger.error(`💔 [${action}] ${e.message} ${message}`))
+}
+
+export const star = (action: Action, message: string) => {
+  logger.info(`⭐️ [${action}] ${message}`)
+}
+
+export const pending = (action: Action, message: string) => {
+  logger.info(`⏳ [${action}] ${message}`)
 }
 
 export const debug = (action: Action, message: Record<any, any>, serialize?: boolean) => {
