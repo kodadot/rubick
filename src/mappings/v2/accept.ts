8c585d1d08810f1ddc1a70c43a89aeaea3aa65ae
Viki Val <viktorko99@gmail.com>
2023-03-20 16:59:12 +0100

    :loud_sound: in src/mappings/v2/accept.ts

--- src/mappings/v2/accept.ts
+++ src/mappings/v2/accept.ts
@@ -7,7 +7,7 @@ import { NFTEntity, Resource } from '../../model'
 import { createEvent } from '../shared/event'
 import { unwrap } from '../utils'
 import { isOwnerOrElseError } from '../utils/consolidator'
-import logger, { logError } from '../utils/logger'
+import { error, success } from '../utils/logger'
 import { Action, Context } from '../utils/types'
 import { getAcceptResource } from './getters'
 
@@ -40,13 +40,11 @@ export async function acceptResource(context: Context) {
         throw new Error(`Unknown entity type ${interaction.entity_type}`)
     }
 
-    logger.success(`[${OPERATION}] ${interaction.entity_type}::${interaction.entity_id} in ${nft.id} from ${caller}`)
+    success(OPERATION, `${interaction.entity_type}::${interaction.entity_id} in ${nft.id} from ${caller}`)
     await context.store.save(nft)
     await createEvent(nft, Action.ACCEPT, { blockNumber, caller, timestamp, version }, `${interaction.entity_type}::${interaction.entity_id}`, context.store)
 
   } catch (e) {
-    logError(e, (e) =>
-      logger.warn(`[${OPERATION}] ${e.message} ${JSON.stringify(interaction)}`)
-    )
+    error(e, OPERATION, JSON.stringify(interaction))
   }
 }
