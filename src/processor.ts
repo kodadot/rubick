d12dbb83edb41e6000167000aae4552a637893c7
Viki Val <viktorko99@gmail.com>
2023-03-20 15:55:34 +0100

    :wrench: smaller testable dataset

--- src/processor.ts
+++ src/processor.ts
@@ -5,8 +5,8 @@ import * as mappings from './mappings'
 
 const processor = new SubstrateProcessor(new Database())
 
-const STARTING_BLOCK = 10269144; // 8788586
-const ENDING_BLOCK = undefined; // 16261119;
+const STARTING_BLOCK = 17120405; // 8788586
+const ENDING_BLOCK = 17120462; // 16261119;
 
 processor.setTypesBundle('kusama');
 // processor.setBlockRange({ from: 5756453 });
