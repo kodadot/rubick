ed592ea3f7b07bda47953c372aa5cc0cd5a5c2ba
Matej Holicky <10matejholicky@gmail.com>
2023-04-06 13:14:14 +0200

    feat: Add resources on lastEvent query for rmrk2

--- src/server-extension/query/event.ts
+++ src/server-extension/query/event.ts
@@ -1,47 +1,68 @@
-export const buyEvent = `SELECT
-    COUNT(e.*) as count,
-    COALESCE(MAX(e.meta::decimal), 0) as max
-    FROM event e
-    LEFT JOIN nft_entity ne on ne.id = e.nft_id
-    WHERE e.interaction = 'BUY' AND ne.collection_id = $1;`;
+export const buyEvent = `
+  SELECT COUNT(e.*) AS COUNT,
+        COALESCE(MAX(e.meta::decimal), 0) AS MAX
+  FROM event e
+  LEFT JOIN nft_entity ne ON ne.id = e.nft_id
+  WHERE e.interaction = 'BUY'
+    AND ne.collection_id = $1;
+`;
 
-export const collectionEventHistory = (
-  idList: string,
-  dateRange: string
-) => `SELECT
-	ce.id as id,
-	DATE(e.timestamp),
-	count(e)
-FROM nft_entity ne
-JOIN collection_entity ce on ce.id = ne.collection_id
-JOIN event e on e.nft_id = ne.id
-WHERE e.interaction = 'BUY'
-and ce.id in (${idList})
-${dateRange}
-GROUP BY ce.id, DATE(e.timestamp)
-ORDER BY DATE(e.timestamp)`;
+export const collectionEventHistory = (idList: string, dateRange: string) => `
+  SELECT ce.id AS id,
+       DATE(e.timestamp),
+       count(e)
+  FROM nft_entity ne
+  JOIN collection_entity ce ON ce.id = ne.collection_id
+  JOIN event e ON e.nft_id = ne.id
+  WHERE e.interaction = 'BUY'
+    AND ce.id in (${idList}) 
+    ${dateRange}
+  GROUP BY ce.id,
+          DATE(e.timestamp)
+  ORDER BY DATE(e.timestamp)
+`;
 
-export const lastEventQuery = (whereCondition: string) => `SELECT
-    DISTINCT ne.id as id,
-    ne.name as name,
-    ne.issuer as issuer,
-    ne.metadata as metadata,
-    e.current_owner,
-    me.image as image,
-    me.animation_url,
-    MAX(e.timestamp) as timestamp,
-    MAX(e.meta::decimal) as value,
-    ne.collection_id as collection_id,
-    ce.name as collection_name
-
-FROM event e
-    JOIN nft_entity ne on e.nft_id = ne.id
-    LEFT join metadata_entity me on me.id = ne.metadata
-    LEFT JOIN collection_entity ce on ne.collection_id = ce.id
-WHERE
-    e.interaction = $1
-    AND ne.burned = false
+export const lastEventQuery = (whereCondition: string) => `
+  SELECT DISTINCT ne.id AS id,
+                ne.name AS name,
+                ne.issuer AS issuer,
+                ne.metadata AS metadata,
+                e.current_owner,
+                me.image AS image,
+                me.animation_url,
+                MAX(e.timestamp) AS timestamp,
+                MAX(e.meta::decimal) AS value,
+                ne.collection_id AS collection_id,
+                ce.name AS collection_name
+  FROM event e
+  JOIN nft_entity ne ON e.nft_id = ne.id
+  LEFT JOIN metadata_entity me ON me.id = ne.metadata
+  LEFT JOIN collection_entity ce ON ne.collection_id = ce.id
+  WHERE e.interaction = $1
+    AND ne.burned = FALSE 
     ${whereCondition}
-GROUP BY ne.id, me.id, e.current_owner, me.image, ce.name
-ORDER BY MAX(e.timestamp) DESC
-LIMIT $2 OFFSET $3`;
+  GROUP BY ne.id,
+          me.id,
+          e.current_owner,
+          me.image,
+          ce.name
+  ORDER BY MAX(e.timestamp) DESC
+  LIMIT $2
+  OFFSET $3
+`;
+
+export const resourcesByNFT = (nftId: string) => `
+  SELECT r.id as id,
+        r.src as src,
+        r.metadata as metadata,
+        r.slot as slot,
+        r.thumb as thumb,
+        r.priority as priority,
+        r.pending as pending,
+        r.meta_id as meta_id,
+        r.nft_id as nft_id
+  FROM resource r
+  WHERE r.nft_id = '${nftId}'
+  LIMIT $1
+  OFFSET $2
+  `;
--- src/server-extension/resolvers/event.ts
+++ src/server-extension/resolvers/event.ts
@@ -1,34 +1,46 @@
-import { Arg, Query, Resolver } from 'type-graphql'
-import type { EntityManager } from 'typeorm'
-import { NFTEntity } from '../../model/generated'
-import { LastEventEntity } from '../model/event.model'
-import { lastEventQuery } from '../query/event'
+import { Arg, FieldResolver, Query, Resolver, Root } from "type-graphql";
+import type { EntityManager } from "typeorm";
+import { NFTEntity } from "../../model/generated";
+import { LastEventEntity, Resource } from "../model/event.model";
+import { lastEventQuery, resourcesByNFT } from "../query/event";
 import { makeQuery, toSqlInParams } from "../utils";
-import { Interaction } from '../../model'
+import { Interaction } from "../../model";
 import { passionQuery } from "../query/nft";
-import { PassionFeedEntity } from '../model/passion.model'
+import { PassionFeedEntity } from "../model/passion.model";
 
-@Resolver()
+@Resolver((of) => LastEventEntity)
 export class EventResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
   @Query(() => [LastEventEntity])
   async lastEvent(
-    @Arg('interaction', { nullable: true, defaultValue: Interaction.LIST }) interaction: Interaction,
-    @Arg('passionAccount', { nullable: true, }) account: string,
-    @Arg('limit', { nullable: true, defaultValue: 20 }) limit: number,
-    @Arg('offset', { nullable: true, defaultValue: 0 }) offset: number,
+    @Arg("interaction", { nullable: true, defaultValue: Interaction.LIST }) interaction: Interaction,
+    @Arg("passionAccount", { nullable: true }) account: string,
+    @Arg("limit", { nullable: true, defaultValue: 20 }) limit: number,
+    @Arg("offset", { nullable: true, defaultValue: 0 }) offset: number
   ): Promise<[LastEventEntity]> {
+    const passionResult: [PassionFeedEntity] = await makeQuery(this.tx, NFTEntity, passionQuery, [account]);
+    const passionList = passionResult.map((passion) => passion.id);
 
+    const selectFromPassionList =
+      passionList && passionList.length > 0 ? `AND ne.issuer in (${toSqlInParams(passionList)})` : "";
+    const result: [LastEventEntity] = await makeQuery(this.tx, NFTEntity, lastEventQuery(selectFromPassionList), [
+      interaction,
+      limit,
+      offset,
+    ]);
 
-    const passionResult: [PassionFeedEntity] = await makeQuery(this.tx, NFTEntity, passionQuery, [account])
-    const passionList = passionResult.map(passion => passion.id)
-
-    const selectFromPassionList = passionList && passionList.length > 0
-    ? `AND ne.issuer in (${toSqlInParams(passionList)})`
-    : ''
-    const result: [LastEventEntity] = await makeQuery(this.tx, NFTEntity, lastEventQuery(selectFromPassionList), [interaction, limit, offset])
-    return result
+    return result;
   }
 
+  @FieldResolver(() => [Resource])
+  async resources(
+    @Root() event: LastEventEntity,
+    @Arg("limit", { nullable: true, defaultValue: 20 }) limit: number,
+    @Arg("offset", { nullable: true, defaultValue: 0 }) offset: number
+  ) {
+    const result: [Resource] = await makeQuery(this.tx, NFTEntity, resourcesByNFT(String(event.id)), [limit, offset]);
+
+    return result;
+  }
 }
