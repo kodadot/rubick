a622bbe0bd03833410d23b2d4072eb0600769331
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:48 +0200

    :broom: eslint and prettier in src/mappings/shared/list.ts

--- src/mappings/shared/list.ts
+++ src/mappings/shared/list.ts
@@ -3,7 +3,12 @@ import { Optional } from '@kodadot1/metasquid/types'
 
 import { NFTEntity } from '../../model'
 import { unwrap } from '../utils'
-import { isMoreTransferable, isOwnerOrElseError, isPositiveOrElseError, validateInteraction } from '../utils/consolidator'
+import {
+  isMoreTransferable,
+  isOwnerOrElseError,
+  isPositiveOrElseError,
+  validateInteraction,
+} from '../utils/consolidator'
 import { getInteraction } from '../utils/getters'
 import { error, success } from '../utils/logger'
 import { Action, Context, RmrkInteraction } from '../utils/types'
@@ -15,7 +20,10 @@ export async function list(context: Context) {
   let interaction: Optional<RmrkInteraction> = null
 
   try {
-    const { value, caller, timestamp, blockNumber, version } = unwrap(context, getInteraction);
+    const { value, caller, timestamp, blockNumber, version } = unwrap(
+      context,
+      getInteraction
+    )
     interaction = value
     const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
     validateInteraction(nft, interaction)
@@ -31,7 +39,13 @@ export async function list(context: Context) {
     success(OPERATION, `${nft.id} from ${caller}`)
     await context.store.save(nft)
     const event = nft.price === 0n ? Action.UNLIST : Action.LIST
-    await createEvent(nft, event, { blockNumber, caller, timestamp, version }, String(price), context.store)
+    await createEvent(
+      nft,
+      event,
+      { blockNumber, caller, timestamp, version },
+      String(price),
+      context.store
+    )
   } catch (e) {
     error(e, OPERATION, JSON.stringify(interaction))
   }
