914317a11decc0cc5061b30b60b34dc12e71adc8
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:48 +0200

    :broom: eslint and prettier in src/mappings/shared/mint.ts

--- src/mappings/shared/mint.ts
+++ src/mappings/shared/mint.ts
@@ -1,34 +1,33 @@
-import {
-  CollectionEntity, NFTEntity
-} from '../../model/generated'
-
 import { plsBe, real } from '@kodadot1/metasquid/consolidator'
 import { getOrFail as get } from '@kodadot1/metasquid/entity'
 import md5 from 'md5'
+import { CollectionEntity, NFTEntity } from '../../model/generated'
 import { unwrap } from '../utils'
 import { isOwnerOrElseError } from '../utils/consolidator'
 import { create } from '../utils/entity'
 import { getCreateToken } from '../utils/getters'
 import { error, success } from '../utils/logger'
-import {
-  Action, Context, getNftId, NFT,
-  Optional
-} from '../utils/types'
+import { Action, Context, getNftId, NFT, Optional } from '../utils/types'
 import { createEvent } from './event'
 import { handleMetadata } from './metadata'
 
 const OPERATION = Action.MINT
 
 // TODO: MINT IS NOT CORRECTLY IMPLEMENTED
-export async function mintItem(
-  context: Context,
-): Promise<void> {
+export async function mintItem(context: Context): Promise<void> {
   let nft: Optional<NFT> = null
   try {
-    const { value, caller, timestamp, blockNumber, version } = unwrap(context, getCreateToken);
+    const { value, caller, timestamp, blockNumber, version } = unwrap(
+      context,
+      getCreateToken
+    )
     nft = value as NFT
     plsBe(real, nft.collection)
-    const collection = await get<CollectionEntity>(context.store, CollectionEntity, nft.collection)
+    const collection = await get<CollectionEntity>(
+      context.store,
+      CollectionEntity,
+      nft.collection
+    )
     isOwnerOrElseError(collection, caller)
     const id = getNftId(nft, blockNumber)
     // const entity = await get<NFTEntity>(context.store, NFTEntity, id) // TODO: check if exists
@@ -54,11 +53,15 @@ export async function mintItem(
     final.pending = false
 
     collection.updatedAt = timestamp
-    collection.nftCount += 1 
-    collection.supply += 1 
+    collection.nftCount += 1
+    collection.supply += 1
 
     if (final.metadata) {
-      const metadata = await handleMetadata(final.metadata, final.name, context.store)
+      const metadata = await handleMetadata(
+        final.metadata,
+        final.name,
+        context.store
+      )
       final.meta = metadata
       final.image = metadata?.image
       final.media = metadata?.animationUrl
@@ -67,8 +70,13 @@ export async function mintItem(
     success(OPERATION, `${final.id} from ${caller}`)
     await context.store.save(final)
     await context.store.save(collection)
-    await createEvent(final, Action.MINT, { blockNumber, caller, timestamp, version }, '', context.store)
-
+    await createEvent(
+      final,
+      Action.MINT,
+      { blockNumber, caller, timestamp, version },
+      '',
+      context.store
+    )
   } catch (e) {
     error(e, OPERATION, JSON.stringify(nft))
   }
