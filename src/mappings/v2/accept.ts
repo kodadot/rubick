f05c61b92ec4f397ba71abae35b67a6689d4af4d
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/v2/accept.ts

--- src/mappings/v2/accept.ts
+++ src/mappings/v2/accept.ts
@@ -17,33 +17,54 @@ export async function acceptResource(context: Context) {
   let interaction: Optional<Accept> = null
 
   try {
-    const { value: interaction, caller, timestamp, blockNumber, version } = unwrap(context, getAcceptResource);
+    const {
+      value: interaction,
+      caller,
+      timestamp,
+      blockNumber,
+      version,
+    } = unwrap(context, getAcceptResource)
     const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
     plsNotBe(burned, nft)
     isOwnerOrElseError(nft, caller)
-    
 
     switch (interaction.entity_type) {
       case 'NFT':
-        const pendingNFT = await get<NFTEntity>(context.store, NFTEntity, interaction.entity_id)
+        const pendingNFT = await get<NFTEntity>(
+          context.store,
+          NFTEntity,
+          interaction.entity_id
+        )
         plsNotBe(burned, nft)
         pendingNFT.updatedAt = timestamp
         pendingNFT.pending = false
         await context.store.save(nft)
-        break;
+        break
       case 'RES':
-        const res = await get<Resource>(context.store, Resource, interaction.entity_id)
+        const res = await get<Resource>(
+          context.store,
+          Resource,
+          interaction.entity_id
+        )
         res.pending = false
         await context.store.save(res)
-        break;
+        break
       default:
         throw new Error(`Unknown entity type ${interaction.entity_type}`)
     }
 
-    success(OPERATION, `${interaction.entity_type}::${interaction.entity_id} in ${nft.id} from ${caller}`)
+    success(
+      OPERATION,
+      `${interaction.entity_type}::${interaction.entity_id} in ${nft.id} from ${caller}`
+    )
     await context.store.save(nft)
-    await createEvent(nft, Action.ACCEPT, { blockNumber, caller, timestamp, version }, `${interaction.entity_type}::${interaction.entity_id}`, context.store)
-
+    await createEvent(
+      nft,
+      Action.ACCEPT,
+      { blockNumber, caller, timestamp, version },
+      `${interaction.entity_type}::${interaction.entity_id}`,
+      context.store
+    )
   } catch (e) {
     error(e, OPERATION, JSON.stringify(interaction))
   }
