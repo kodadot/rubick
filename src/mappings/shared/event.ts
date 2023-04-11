54a2758f4840debf0a86938273809bee7f3afea7
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:48 +0200

    :broom: eslint and prettier in src/mappings/shared/event.ts

--- src/mappings/shared/event.ts
+++ src/mappings/shared/event.ts
@@ -7,15 +7,27 @@ import { eventId } from '../utils/helper'
 import logger, { logError } from '../utils/logger'
 import { eventFrom, BaseCall } from '../utils/types'
 
-export async function createEvent(final: NFTEntity, interaction: Interaction, call: BaseCall, meta: string, store: Store, currentOwner?: string) {
+export async function createEvent(
+  final: NFTEntity,
+  interaction: Interaction,
+  call: BaseCall,
+  meta: string,
+  store: Store,
+  currentOwner?: string
+) {
   try {
     const newEventId = eventId(final.id, interaction)
-    const event = create<Event>(Event, newEventId, eventFrom(interaction, call, meta, currentOwner))
+    const event = create<Event>(
+      Event,
+      newEventId,
+      eventFrom(interaction, call, meta, currentOwner)
+    )
     event.nft = final
     event.version = toVersion(call.version || final.version)
     await store.save(event)
   } catch (e) {
-    logError(e, (e) => logger.warn(`[[${interaction}]]: ${final.id} Reason: ${e.message}`))
+    logError(e, (e) =>
+      logger.warn(`[[${interaction}]]: ${final.id} Reason: ${e.message}`)
+    )
   }
-  
 }
