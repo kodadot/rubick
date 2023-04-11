736e97e146095b59576f107d3af47ab597542509
Viki Val <viktorko99@gmail.com>
2023-04-11 23:19:28 +0200

    :zap: changeissuer also for base

--- src/mappings/v2/change.ts
+++ src/mappings/v2/change.ts
@@ -1,31 +1,34 @@
-import { plsBe, real } from '@kodadot1/metasquid/consolidator'
+import { plsBe } from '@kodadot1/metasquid/consolidator'
 import { getOrFail as get } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
 
-import { CollectionEntity } from '../../model'
+import { ChangeIssuer } from '@kodadot1/minimark/v2'
+import { Base, CollectionEntity } from '../../model'
 import { unwrap } from '../utils'
 import { isOwnerOrElseError, withMeta } from '../utils/consolidator'
-import { getInteraction } from '../utils/getters'
 import { error, success } from '../utils/logger'
-import { Action, Context, RmrkInteraction } from '../utils/types'
+import { Action, Context } from '../utils/types'
+import { getChangeIssuer } from './getters'
 
 const OPERATION = Action.CHANGEISSUER
 
-// TODO: can also change BASE in V2 (not implemented yet)
 export async function changeIssuer(context: Context) {
-  let interaction: Optional<RmrkInteraction> = null
+  let interaction: Optional<ChangeIssuer> = null
 
   try {
-    const { value, caller } = unwrap(context, getInteraction)
+    const { value, caller } = unwrap(context, getChangeIssuer)
     interaction = value
     plsBe(withMeta, interaction)
-    const collection = await get<CollectionEntity>(context.store, CollectionEntity, interaction.id)
-    plsBe<CollectionEntity>(real, collection)
-    isOwnerOrElseError(collection, caller)
-    collection.currentOwner = interaction.value
 
-    success(OPERATION, `${collection.id} from ${caller}`)
-    await context.store.save(collection)
+    const isBase = interaction.id.startsWith('base')
+    const entity = isBase
+      ? await get<Base>(context.store, Base, interaction.id)
+      : await get<CollectionEntity>(context.store, CollectionEntity, interaction.id)
+    isOwnerOrElseError(entity, caller)
+    entity.currentOwner = interaction.value
+
+    success(OPERATION, `${entity.id} from ${caller}`)
+    await context.store.save(entity)
   } catch (e) {
     error(e, OPERATION, JSON.stringify(interaction))
   }
