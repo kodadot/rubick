563e068b92d40ff710f946962a0dbfc8d2c3f9a1
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/utils/logger.ts

--- src/mappings/utils/logger.ts
+++ src/mappings/utils/logger.ts
@@ -1,15 +1,15 @@
 import { serializer } from '@kodadot1/metasquid'
 import { logger } from '@kodadot1/metasquid/logger'
-import { Action as MetaAction  } from '../utils/types'
 import { Interaction } from '@kodadot1/minimark/v1'
+import { Action as MetaAction } from '../utils/types'
 
 type Action = MetaAction | Interaction
 
-type ErrorCallback = (error: Error) => void;
+type ErrorCallback = (error: Error) => void
 
 export const logError = (e: Error | unknown, cb: ErrorCallback) => {
   if (e instanceof Error) {
-    cb(e);
+    cb(e)
   }
 }
 
@@ -25,8 +25,18 @@ export const pending = (action: Action, message: string) => {
   logger.info(`⏳ [${action}] ${message}`)
 }
 
-export const debug = (action: Action, message: Record<any, any>, serialize?: boolean) => {
-  logger.debug(`[${action}] ${JSON.stringify(message, serialize ?  serializer : undefined, 2)}`)
+export const debug = (
+  action: Action,
+  message: Record<any, any>,
+  serialize?: boolean
+) => {
+  logger.debug(
+    `[${action}] ${JSON.stringify(
+      message,
+      serialize ? serializer : undefined,
+      2
+    )}`
+  )
 }
 
-export default logger
+export { logger as default } from '@kodadot1/metasquid/logger'
