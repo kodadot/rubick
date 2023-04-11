34ad64e2a2b952f945a99f56456b15e8408c92a3
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/utils/verbose.ts

--- src/mappings/utils/verbose.ts
+++ src/mappings/utils/verbose.ts
@@ -3,13 +3,13 @@ import { create, EntityWithId, get } from '@kodadot1/metasquid/entity'
 import { EntityConstructor } from '@kodadot1/metasquid/types'
 import { Context } from './types'
 
-export async function createUnlessNotExist<T extends EntityWithId>(id: string, object: EntityConstructor<T>, { store }: Context) {
+export async function createUnlessNotExist<T extends EntityWithId>(
+  id: string,
+  object: EntityConstructor<T>,
+  { store }: Context
+) {
   plsBe<string>(real, id)
-  const entity = await get<T>(
-    store,
-    object,
-    id
-  )
+  const entity = await get<T>(store, object, id)
   plsNotBe<T>(real, entity as T)
 
   return create<T>(object, id, {})
