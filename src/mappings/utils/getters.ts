fce23ba650f93196865207984588c232eeed0463
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/utils/getters.ts

--- src/mappings/utils/getters.ts
+++ src/mappings/utils/getters.ts
@@ -1,4 +1,8 @@
-import { InteractionValue, UnwrappedRemark, unwrapRemark } from '@kodadot1/minimark/v1'
+import {
+  InteractionValue,
+  UnwrappedRemark,
+  unwrapRemark,
+} from '@kodadot1/minimark/v1'
 import { SystemRemarkCall } from '../../types/calls'
 import { extractExtra } from './extract'
 import { ensureInteraction } from './helper'
@@ -9,8 +13,10 @@ export function getRemarkString(ctx: Context): string {
   return remark.toString()
 }
 
-export function getRemark<T = InteractionValue>(ctx: Context): UnwrappedRemark<T | InteractionValue> {
-  const remark  = getRemarkString(ctx)
+export function getRemark<T = InteractionValue>(
+  ctx: Context
+): UnwrappedRemark<T | InteractionValue> {
+  const remark = getRemarkString(ctx)
   return unwrapRemark<T>(remark)
 }
 
@@ -22,13 +28,17 @@ export function getCreateToken(ctx: Context): UnwrappedRemark<NFT> {
   return getRemark<any>(ctx) as UnwrappedRemark<NFT>
 }
 
-export function getInteraction(ctx: Context): UnwrappedRemark<InteractionValue> {
+export function getInteraction(
+  ctx: Context
+): UnwrappedRemark<InteractionValue> {
   const { value, ...rest } = getRemark<InteractionValue>(ctx)
   return { ...rest, value: ensureInteraction(value) }
 }
 
-export function getInteractionWithExtra(ctx: Context): UnwrappedRemark<InteractionValue> & InteractionExtra {
+export function getInteractionWithExtra(
+  ctx: Context
+): UnwrappedRemark<InteractionValue> & InteractionExtra {
   const { value, ...rest } = getRemark<InteractionValue>(ctx)
   const extra = extractExtra(ctx)
-  return { ...rest, value: ensureInteraction(value), extra  }
+  return { ...rest, value: ensureInteraction(value), extra }
 }
