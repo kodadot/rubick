63e098929900107039280188caceaa9036fb97b0
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/v1/index.ts

--- src/mappings/v1/index.ts
+++ src/mappings/v1/index.ts
@@ -1,68 +1,71 @@
-
-import { unwrapRemark } from '@kodadot1/minimark/v1'
+import { unwrapRemark, Interaction } from '@kodadot1/minimark/v1'
 import { unwrap } from '../utils'
 
-import { Interaction } from '@kodadot1/minimark/v1'
-import { changeIssuer, burn as consume, createCollection, emote, list, send } from '../shared'
+import {
+  changeIssuer,
+  burn as consume,
+  createCollection,
+  emote,
+  list,
+  send,
+} from '../shared'
 import { mintItem } from '../shared/mint'
 import logger, { pending } from '../utils/logger'
-import {
-  Context,
-  RmrkInteraction
-} from '../utils/types'
+import { Context, RmrkInteraction } from '../utils/types'
 import { buy } from './buy'
 
+export async function mainFrame(
+  remark: string,
+  context: Context
+): Promise<void> {
+  const base = unwrap(context, (_: Context) => ({ value: remark }))
+  try {
+    const { interaction: event, version } = unwrapRemark<RmrkInteraction>(
+      remark.toString()
+    )
+    pending(event, base.blockNumber)
 
-export async function mainFrame(remark: string, context: Context): Promise<void> {
-    const base = unwrap(context, (_: Context) => ({ value: remark }))
-    try {
-      const { interaction: event, version } = unwrapRemark<RmrkInteraction>(remark.toString())
-      pending(event, base.blockNumber)
+    if (version === '2.0.0') {
+      logger.error(
+        `[RMRK::2.0.0] should be handled by different handler ${remark}`
+      )
+      return
+    }
 
-      if (version === '2.0.0') {
-        logger.error(`[RMRK::2.0.0] should be handled by different handler ${remark}`)
-        return;
-      }
-
-      switch (event) {
-        case Interaction.MINT:
-          await createCollection(context)
-          break
-        case Interaction.MINTNFT:
-          await mintItem(context)
-          break
-        case Interaction.SEND:
-          await send(context)
-          break
-        case Interaction.BUY:
-          await buy(context)
-          break
-        case Interaction.CONSUME:
-          await consume(context)
-          break
-        case Interaction.LIST:
-          await list(context)
-          break
-        case Interaction.CHANGEISSUER:
-          await changeIssuer(context)
-          break
-        case Interaction.EMOTE:
-          await emote(context)
-          break
-        default:
-          logger.warn(
-            `[SKIP] ${event}::${base.value}::${base.blockNumber}`
-          )
-      }
-    } catch (e) {
-      logger.warn(
-        `[MALFORMED]
+    switch (event) {
+      case Interaction.MINT:
+        await createCollection(context)
+        break
+      case Interaction.MINTNFT:
+        await mintItem(context)
+        break
+      case Interaction.SEND:
+        await send(context)
+        break
+      case Interaction.BUY:
+        await buy(context)
+        break
+      case Interaction.CONSUME:
+        await consume(context)
+        break
+      case Interaction.LIST:
+        await list(context)
+        break
+      case Interaction.CHANGEISSUER:
+        await changeIssuer(context)
+        break
+      case Interaction.EMOTE:
+        await emote(context)
+        break
+      default:
+        logger.warn(`[SKIP] ${event}::${base.value}::${base.blockNumber}`)
+    }
+  } catch (e) {
+    logger.warn(
+      `[MALFORMED]
          [BLOCK] ${base.blockNumber}
          [ERROR] ${(e as Error).message}
          [RMRK] ${base.value}`
-      )
-    }
+    )
   }
-
-
-
+}
