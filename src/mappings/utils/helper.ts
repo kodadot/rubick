58f58d2c444aa3604f8c6b53b8d659043f353cd6
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/utils/helper.ts

--- src/mappings/utils/helper.ts
+++ src/mappings/utils/helper.ts
@@ -8,14 +8,23 @@ export { isEmpty, trim, trimAll } from '@kodadot1/minimark/utils'
 export { toBaseId as baseId } from '@kodadot1/minimark/v2'
 
 // Lenght is 47 or 48
-export const SS58_REGEX = /^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{47}$/
+export const SS58_REGEX = /^[1-9A-HJ-NP-Za-km-z]{47}$/
 
+export const emoteId = (
+  { id, value: metadata }: RmrkInteraction,
+  caller: string
+) => `${id}-${metadata}-${caller}`
 
-export const emoteId = ({ id, value: metadata }: RmrkInteraction, caller: string) => `${id}-${metadata}-${caller}`
+export const eventId = (id: string, event: Action) =>
+  `${id}-${event}${nanoid()}`
 
-export const eventId = (id: string, event: Action) => `${id}-${event}${nanoid()}`
-
-export const ensureInteraction = ({ id, value: metadata }: RmrkInteraction): RmrkInteraction  => ({ id: trim(id), value: trimAll(metadata) })
+export const ensureInteraction = ({
+  id,
+  value: metadata,
+}: RmrkInteraction): RmrkInteraction => ({
+  id: trim(id),
+  value: trimAll(metadata),
+})
 
 export const collectionIdFrom = (nftId: string) => {
   const [_blockNumber, id, symbol] = nftId.split('-')
@@ -27,9 +36,8 @@ export function ensure<T>(value: any): T {
 }
 
 export function camelCase(str: string): string {
-  return str.replace(/([_][a-z])/ig, ($1) => {
-    return $1.toUpperCase()
-      .replace('_', '')
+  return str.replace(/(_[a-z])/gi, ($1) => {
+    return $1.toUpperCase().replace('_', '')
   })
 }
 
@@ -37,8 +45,10 @@ export function onlyValue(call: ArchiveCallWithOptionalValue): any {
   return call?.value
 }
 
-//Uint8Array 
-export function addressOf(address: ArchiveCallWithOptionalValue | string): string {
+// Uint8Array
+export function addressOf(
+  address: ArchiveCallWithOptionalValue | string
+): string {
   const value = typeof address === 'string' ? address : onlyValue(address)
   return ss58.codec('kusama').encode(decodeHex(value))
 }
@@ -47,7 +57,7 @@ export function isAddress(address: string): boolean {
   try {
     ss58.codec('kusama').decode(address)
     return true
-  } catch (e) {
+  } catch {
     return false
   }
 }
@@ -59,9 +69,9 @@ export function isDummyAddress(address?: string): boolean {
 
 // RMRK R&D team
 export const isValidAddressPolkadotAddress = (address: string) => {
-  return address && !address.includes("-");
-};
+  return address && !address.includes('-')
+}
 
 export function metadataOf({ metadata }: { metadata: string }): string {
-  return metadata ?? '';
+  return metadata ?? ''
 }
