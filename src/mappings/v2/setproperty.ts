3fb521b3c5f5e7dafcccda92e1f75859af20a1f6
Viki Val <viktorko99@gmail.com>
2023-04-11 17:44:49 +0200

    :broom: eslint and prettier in src/mappings/v2/setproperty.ts

--- src/mappings/v2/setproperty.ts
+++ src/mappings/v2/setproperty.ts
@@ -13,20 +13,26 @@ export async function setProperty(context: Context) {
 
   try {
     const getter = getSetProperty
-    const { value: interaction, caller, timestamp, blockNumber, version } = unwrap(context, getter);    
+    const {
+      value: interaction,
+      caller,
+      timestamp,
+      blockNumber,
+      version,
+    } = unwrap(context, getter)
 
     // logger.success(`[${OPERATION}] NEW PRIORITY ${interaction.value} for ${nft.id} from ${caller}`)
 
     // TODO: add logic for accepting resource
 
-    logger.info(`[${OPERATION}] < ${interaction.name}, ${interaction.value} > for ${interaction.id} from ${caller}`)
+    logger.info(
+      `[${OPERATION}] < ${interaction.name}, ${interaction.value} > for ${interaction.id} from ${caller}`
+    )
     // await context.store.save(nft)
     // await createEvent(nft, OPERATION, { blockNumber, caller, timestamp, version }, `${interaction.value.at(0)}`, context.store)
-
   } catch (e) {
     logError(e, (e) =>
       logger.warn(`[${OPERATION}] ${e.message} ${JSON.stringify(interaction)}`)
     )
   }
 }
-
