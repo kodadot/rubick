3caedf6f6ca3bf8872d25d46c511367b5cee6e6e
Viki Val <viktorko99@gmail.com>
2023-04-13 22:39:36 +0200

    :zap: implemented equippable

--- src/mappings/v2/equippable.ts
+++ src/mappings/v2/equippable.ts
@@ -1,9 +1,9 @@
 import { burned, plsBe, plsNotBe, real } from '@kodadot1/metasquid/consolidator'
-import { getOrFail as get } from '@kodadot1/metasquid/entity'
+import { getOrFail as get, getWith } from '@kodadot1/metasquid/entity'
 import { Optional } from '@kodadot1/metasquid/types'
-import { Equippable, Interaction, resolveEquippable } from '@kodadot1/minimark/v2'
+import { Equippable, Interaction, resolveEquippable, toPartId } from '@kodadot1/minimark/v2'
 
-import { NFTEntity } from '../../model'
+import { Base, NFTEntity, Part } from '../../model'
 import { createEvent } from '../shared/event'
 import { unwrap } from '../utils'
 import { isOwnerOrElseError } from '../utils/consolidator'
@@ -20,33 +20,39 @@ export async function equippable(context: Context) {
     const getE = getAs<Interaction.EQUIPPABLE>()
     const { value: equip, caller, timestamp, blockNumber, version } = unwrap(context, getE)
     interaction = equip
-    const nft = await get<NFTEntity>(context.store, NFTEntity, interaction.id)
-    plsBe(real, nft)
-    plsNotBe(burned, nft)
-    isOwnerOrElseError(nft, caller)
-    nft.updatedAt = timestamp
+    const id = toPartId(interaction.id, interaction.slot)
+    const part = await getWith<Part>(context.store, Part, id, { base: true })
+    isOwnerOrElseError(part.base, caller)
+    // base.updatedAt = timestamp
 
     const equipOption = resolveEquippable(interaction.value)
+
+    if (!part.equippable) {
+      part.equippable = []
+    }
 
     // TODO: add logic for EQUIPing resource
     switch (equipOption.operation) {
       case '+':
+        part.equippable.push(equipOption.collection)
         break;
       case '-':
+        part.equippable = part.equippable.filter((e) => e !== equipOption.collection)
         break;
       case '*':
+        part.equippable = [equipOption.collection || '*']
         break;
     }
     
-    success(OPERATION, `${nft.id} from ${caller}`)
-    await context.store.save(nft)
-    await createEvent(
-      nft,
-      OPERATION,
-      { blockNumber, caller, timestamp, version },
-      `${interaction.id}::${interaction.slot}`,
-      context.store
-    )
+    success(OPERATION, `${part.id} from ${caller}`)
+    await context.store.save(part)
+    // await createEvent(
+    //   nft,
+    //   OPERATION,
+    //   { blockNumber, caller, timestamp, version },
+    //   `${interaction.id}::${interaction.slot}`,
+    //   context.store
+    // )
   } catch (e) {
     error(e, OPERATION, JSON.stringify(interaction))
   }
