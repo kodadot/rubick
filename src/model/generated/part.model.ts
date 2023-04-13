24f83b8592c0f45d93025c0a1b58da656c7c365b
Viki Val <viktorko99@gmail.com>
2023-04-13 22:22:01 +0200

    :alien: partId is now called name

--- src/model/generated/part.model.ts
+++ src/model/generated/part.model.ts
@@ -1,5 +1,6 @@
 import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_} from "typeorm"
 import {Base} from "./base.model"
+import {MetadataEntity} from "./metadataEntity.model"
 import {PartType} from "./_partType"
 
 @Entity_()
@@ -23,6 +24,10 @@ export class Part {
 
     @Column_("text", {nullable: true})
     metadata!: string | undefined | null
+
+    @Index_()
+    @ManyToOne_(() => MetadataEntity, {nullable: true})
+    meta!: MetadataEntity | undefined | null
 
     @Column_("text", {nullable: true})
     src!: string | undefined | null
