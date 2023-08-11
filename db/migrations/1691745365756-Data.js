66c86386e1bf680790d5b4a1fb02e59c67b0aba9
Viki Val <viktorko99@gmail.com>
2023-08-11 12:23:18 +0300

    :bug: volume by default is zero

--- db/migrations/1691745365756-Data.js
+++ db/migrations/1691745365756-Data.js
@@ -3,6 +3,7 @@ module.exports = class Data1691745365756 {
 
     async up(db) {
         await db.query(`ALTER TABLE "collection_entity" ADD "volume" numeric`)
+        await db.query(`UPDATE "collection_entity" SET "volume" = 0`)
     }
 
     async down(db) {
